c59de524adff61a4647400bc2ddd62c5d1eea475
Parthadeb Mondal <Parthadev76@gmail.com>
2023-09-23 12:39:25 +0600

    Dhaka prokash mobile menu

--- components/MobileHeader/Dropdown.vue
+++ components/MobileHeader/Dropdown.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-        
+    <div :class="`${mobileMenuStatus ? 'left-0 fixed' : 'absolute -left-[415px]'} duration-200 bg-white h-screen max-w-full md:hidden right-0 top-12`">
+        <div class="px-6 py-6 ">
+            <h2>1</h2>
+        <h2>2</h2>
+        <h2>3</h2>
+        <h2>4</h2>
+        <h2>1</h2>
+        <h2>1</h2>
+        <h2>1</h2>
+        </div>
     </div>
 </template>
 
 <script setup>
-
+    const {mobileMenuStatus} = defineProps(['mobileMenuStatus'])
 </script>
 
 <style lang="scss" scoped>
--- components/MobileHeader/Top.vue
+++ components/MobileHeader/Top.vue
@@ -1,20 +1,29 @@
 <template>
-    <div class="flex justify-between items-center md:hidden shadow-md">
-        <div class="flex gap-6 px-2 py-2 items-center justify-center">
-            <Icon class="text-3xl cursor-pointer hover:bg-[#f7f7f7]" name="ic:outline-menu" />
-            <nuxt-img class="mx-auto" src="/assets/img/logo.png"  alt="Dhaka Prokash" />
+    <div>
+        <div class="flex justify-between items-center md:hidden shadow-md fixed z-50 bg-white top-0 left-0 right-0">
+            <div class="flex gap-6 px-2 py-2 items-center justify-center">
+                <Icon @click="mobileMenuToggle" class="text-3xl cursor-pointer hover:bg-[#f7f7f7]" name="ic:outline-menu" />
+                <nuxt-img class="mx-auto" src="/assets/img/logo.png" alt="Dhaka Prokash" />
+            </div>
+
+            <div class="flex gap-3 px-2">
+                <NuxtLink class="border px-2 font-semibold text-blue-700" to="/">EN</NuxtLink>
+                <NuxtLink to="/" class="border px-2 font-semibold text-blue-700">E-P</NuxtLink>
+            </div>
         </div>
-      
-        <div class="flex gap-3 px-2">
-            <NuxtLink class="border px-2 font-semibold text-blue-700" to="/">EN</NuxtLink>
-            <NuxtLink to="/" class="border px-2 font-semibold text-blue-700">E-P</NuxtLink>
-        </div>
+        <MobileHeaderDropdown :mobileMenuStatus="mobileMenuStatus" />
     </div>
-    
 </template>
 
 <script setup>
-
+    const mobileMenuStatus = ref(false)
+    const mobileMenuToggle = () => {
+        if (mobileMenuStatus.value === true) {
+            mobileMenuStatus.value = false
+        }else{
+            mobileMenuStatus.value = true
+        }
+    }
 </script>
 
 <style lang="scss" scoped></style>
