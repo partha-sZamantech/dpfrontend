c4e54cf1e8ba09fc8debd1ec76d7aebf58ea0e93
Parthadeb Mondal <Parthadev76@gmail.com>
2023-09-23 18:54:31 +0600

    header fixeing

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,16 +5,21 @@
             <Title>Dhaka Prokash</Title>
             <Meta content="500" http-equiv="refresh" />
         </Head>
+
+        <div :class="`${LogoHeaderScollUp ? '  fixed top-0 left-0 right-0' : 'fixed -top-20 left-0 right-0'} duration-300`">
+            <div class="header-container max-w-[1280px] mx-auto ">
+                <!-- Top Header -->
+                <HeaderTop :scrollDown="scrollDown" :LogoHeaderScollUp="LogoHeaderScollUp" />
+                <!-- Mobile Header -->
+                <!-- Top Header -->
+                <HeaderTopMenu :scrollDown="scrollDown" :counter="counter" />
+            </div>
+        </div>
+
         <div class="main-container max-w-[1280px] mx-auto ">
 
 
-            <!-- Top Header -->
-            <HeaderTop :scrollDown="scrollDown" :LogoHeaderScollUp="LogoHeaderScollUp" />
-            <!-- Mobile Header -->
             <MobileHeaderTop />
-
-            <!-- Top Header -->
-            <HeaderTopMenu :scrollDown="scrollDown" :counter="counter" />
             <slot />
             <FooterContent />
 
@@ -54,20 +59,20 @@ onMounted(() => {
         currentScrollPosition.value = window.scrollY
 
         if (document.documentElement.scrollTop >= scrollPostion.value) {
-                scrollDown.value = true
+            scrollDown.value = true
 
-            } else {
-                scrollDown.value = false
-                // const adsBottomStatus = ref(false)
-            }
+        } else {
+            scrollDown.value = false
+            // const adsBottomStatus = ref(false)
+        }
 
         if (prevScrollPosition.value > currentScrollPosition.value) {
             // Scroll Up
             scrollDown.value = false
             LogoHeaderScollUp.value = true
-        
+
         } else {
-            scrollDown.value = true
+
             // Scroll down
             LogoHeaderScollUp.value = false
         }
