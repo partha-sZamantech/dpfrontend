83bfb14df90f8081ad1e15435094c882f927d526
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-03 13:14:01 +0600

    headline mobile version ok

--- components/Headline.vue
+++ components/Headline.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class=" border">
+    <div class=" border border-red-900">
         <div class="flex items-center">
-            <div class="bg-blue-900 w-[140px] px-2 py-1">
-                <h2 class="text-[16px] text-white">প্রধান শিরোনাম </h2>
+            <div class="bg-red-900 md:w-[140px] w-[100px] px-2 border border-red-900 py-1">
+                <h2 class="text-[12px] md:text-[16px] text-white">প্রধান শিরোনাম </h2>
             </div>
-            <marquee width="100%" direction="left" scrollamount="10">
+            <marquee class="flex-1" width="100%" direction="left" scrollamount="10">
                 <div class="flex items-center">
                     <span class="relative flex h-3 w-3">
                         <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
