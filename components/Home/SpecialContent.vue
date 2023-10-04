dcffcacfe52417fd4dbe3a48525f3b11cf25bd20
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-04 18:14:22 +0600

    Dhaka prokash nuxt home page designing

---
+++ components/Home/SpecialContent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class=" grid grid-cols-12">
+        <!-- Start Special Content Area -->
+        <div class="  col-span-6">
+            <!-- Start Special Content Top -->
+
+            <div class="grid grid-cols-12">
+                <div class=" col-span-8">
+                    <nuxt-img src="/assets/img/banner.jpg" class="w-full" placeholder="/assets/img/logo.png" />
+                </div>
+                <div class="col-span-4">
+                    <div class="flex flex-col gap-2">
+                        <h2 class="text-[#121212] text-2xl font-[600]">দেশের নিট রিজার্ভ এখন ১৮ বিলিয়ন ডলারের নিচে: জাহিদ
+                            হোসেন</h2>
+                        <p class="text-[#555555] text-[16px]">দেশের সামষ্টিক অর্থনৈতিক অস্থিতিশীলতার পেছনে বহিস্থ কারণের
+                            সঙ্গে দেশীয় কারণও আছে বলে মনে করেন জাহিদ হোসেন।</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- End Special Content Top -->
+        </div>
+        <!-- End Special Content Area -->
+        <div class=" col-span-6">
+            <h2>DKfd</h2>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  css: ['~/assets/css/main.css'],
+  css: [
+    '~/assets/css/main.css',
+    '~/assets/css/global.css',
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
