63f64bd40d8872f71620cc5f61b88c18815b8849
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-04 18:53:42 +0600

    home page special category done

--- components/Home/SpecialContent.vue
+++ components/Home/SpecialContent.vue
@@ -1,9 +1,9 @@
 <template>
     <div class=" grid grid-cols-12 py-4 border-t border-t-[#e2e2e2] border-b border-b-[#e2e2e2]">
         <!-- Start Special Content Area -->
-        <div class="  col-span-7">
+        <div class="  col-span-6">
             <!-- Start Special Content Top -->
-            <div class="grid grid-cols-12 gap-4 border-r pr-2">
+            <div class="grid grid-cols-12 gap-2 border-r pr-2 border-r-[#e2e2e2]">
                 <div class=" col-span-8">
                     <nuxt-img src="/assets/img/banner.jpg" class="w-full" placeholder="/assets/img/logo.png" />
                 </div>
@@ -19,10 +19,10 @@
             <!-- End Special Content Top -->
         </div>
         <!-- End Special Content Area -->
-        <div class=" col-span-5">
-            <div class="grid grid-cols-2 gap-3">
-                <div>
-                    <div class="flex flex-col gap-2">
+        <div class=" col-span-6">
+            <div class="grid grid-cols-2 gap-2">
+                <div class="px-2 border-r border-r-[#e2e2e2]">
+                    <div class="flex flex-col">
                         <nuxt-img src="/assets/img/banner.jpg" class="" placeholder="/assets/img/logo.png" />
                         <h2 class="text-[#121212] text-[18px] font-[600]">দেশের নিট রিজার্ভ এখন ১৮ বিলিয়ন ডলারের নিচে: জাহিদ
                             হোসেন</h2>
