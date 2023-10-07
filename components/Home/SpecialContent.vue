508577da278126592f2d84813da491584ac9979d
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-07 17:18:54 +0600

    Responsive speical content

--- components/Home/SpecialContent.vue
+++ components/Home/SpecialContent.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
         <!-- Start Special Content Area 1 -->
-        <div class=" grid grid-cols-12 py-4 border-t border-t-[#e2e2e2] border-b border-b-[#e2e2e2]">
-            <div class="  col-span-6 border-r border-r-[#e2e2e2]">
+        <div class=" grid grid-cols-12 py-4 md:border-t md:border-t-[#e2e2e2] md:border-b md:border-b-[#e2e2e2]">
+            <div class=" col-span-12 md:col-span-6 md:border-r md:border-r-[#e2e2e2]">
                 <!-- Special Content Position 1 -->
                 <NuxtLink to="/" class=" group">
-                    <div class="grid grid-cols-12 gap-2 pr-2">
-                        <div class=" col-span-8 overflow-hidden">
+                    <div class="grid grid-cols-12 gap-2 md:pr-2">
+                        <div class="col-span-12 md:col-span-8 overflow-hidden">
                             <nuxt-img src="https://www.dhakaprokash24.com/media/content/images/2023October/nobel-20231005171906.jpg" class="mx-auto w-full group-hover:scale-110 duration-300"
                                 :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
                         </div>
-                        <div class="col-span-4">
+                        <div class=" col-span-12 md:col-span-4">
                             <div class="flex flex-col gap-2 px-2">
                                 <h2 class="text-[#121212] text-[18px] font-[600] group-hover:text-blue-700">দেশের নিট
                                     রিজার্ভ এখন ১৮ বিলিয়ন ডলারের
@@ -26,13 +26,13 @@
                 <!-- Special Content Position 1 -->
             </div>
 
-            <div class=" col-span-6">
-                <div class="grid grid-cols-2 gap-2">
-                    <div class="px-2 border-r border-r-[#e2e2e2]">
+            <div class=" col-span-12 md:col-span-6">
+                <div class="grid grid-flow-row md:grid-cols-2 gap-2">
+                    <div class="md:px-2 md:border-r md:border-r-[#e2e2e2]">
                         <!-- Special Content Position 2 -->
                         <NuxtLink to="/" class="group">
                             <div class="flex flex-col">
-                                <div class="w-[295px] h-[165px] overflow-hidden">
+                                <div class=" w-full md:w-[295px] md:h-[165px] overflow-hidden">
                                     <nuxt-img src="https://www.dhakaprokash24.com/media/content/images/2023October/nobel-20231005171906.jpg" class="mx-auto w-full group-hover:scale-110 duration-300"
                                 :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
                                 </div>
