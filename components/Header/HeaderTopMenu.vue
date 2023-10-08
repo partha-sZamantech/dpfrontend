1a7902d8918b1f3ca85acc9e65bf87f2504ea08b
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-08 11:43:51 +0600

    header color added

--- components/Header/HeaderTopMenu.vue
+++ components/Header/HeaderTopMenu.vue
@@ -1,6 +1,6 @@
 <template>
-    <div :class="`${scrollDown ? 'fixed top-0 right-0 left-0 shadow-lg' : ''} hidden md:block duration-300 border-t border-b-2 bg-white`">
-        <ul class="flex gap-1 justify-center text-[18px]">
+    <div :class="`${scrollDown ? 'fixed top-0 right-0 left-0 shadow-lg' : ''} hidden md:block shadow-lg shadow-gray-300 duration-300 bg-[#3375af]`">
+        <ul class="flex gap-1 justify-center text-[18px] text-white">
             <li v-for="category in headCategory" :key="category.cat_id">
                 <NuxtLink :to="`/category/${category.cat_slug}`" class="py-2 px-3 block hover:text-red-700">{{ category.cat_name_bn }}</NuxtLink>
             </li>
