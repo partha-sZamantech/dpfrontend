edf407b61ce0d0a37b1a98b933756567dcf93719
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-08 16:51:33 +0600

    Home special content youtube

--- components/Home/SpecialTopContent.vue
+++ components/Home/SpecialTopContent.vue
@@ -93,22 +93,22 @@
                         <!-- <iframe width="518" height="292" src="https://www.youtube.com/embed/{{$spTopFirstVideo->code}}?enablejsapi=1&autoplay=1&mute=1&rel=0&showinfo=1&controls=1&loop=1&playlist={{$spTopFirstVideo->code}}" frameborder="0" allowfullscreen style="width: 100%!important;"></iframe> -->
 
                     </div>
-                    <div class="grid grid-cols-2 place-items-center">
+                    <div class="grid grid-cols-1 md:grid-cols-2 md:place-items-center">
                         <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
-                            <Icon name="simple-icons:youtubemusic" class=" col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
-                            <h4 class="text-[18px] col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                            <Icon name="simple-icons:youtubemusic" class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
                         </NuxtLink>
                         <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
-                            <Icon name="simple-icons:youtubemusic" class=" col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
-                            <h4 class="text-[18px] col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                            <Icon name="simple-icons:youtubemusic" class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
                         </NuxtLink>
                         <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
-                            <Icon name="simple-icons:youtubemusic" class=" col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
-                            <h4 class="text-[18px] col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                            <Icon name="simple-icons:youtubemusic" class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
                         </NuxtLink>
                         <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
-                            <Icon name="simple-icons:youtubemusic" class=" col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
-                            <h4 class="text-[18px] col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                            <Icon name="simple-icons:youtubemusic" class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
                         </NuxtLink>
                    
                         <!-- <NuxtLink to="/" class="flex gap-4 items-center group border-b py-4">
