404bdcdfcc1801b5cc855cd2b36dc85037e92ea5
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-08 16:34:09 +0600

    Header menu designing

--- components/Header/HeaderTopMenu.vue
+++ components/Header/HeaderTopMenu.vue
@@ -1,8 +1,8 @@
 <template>
     <div :class="`${scrollDown ? 'fixed top-0 right-0 left-0 shadow-lg' : ''} hidden md:block shadow-lg shadow-gray-300 duration-300 bg-[#3375af]`">
-        <ul class="flex gap-1 justify-center text-[18px] text-white">
+        <ul class="flex gap-0 justify-center text-[18px] text-white">
             <li v-for="category in headCategory" :key="category.cat_id">
-                <NuxtLink :to="`/category/${category.cat_slug}`" class="py-2 px-3 block hover:text-red-700">{{ category.cat_name_bn }}</NuxtLink>
+                <NuxtLink :to="`/category/${category.cat_slug}`" class="py-2 px-3 block hover:bg-[#284f81] hover:border-b-2 hover:border-b-white border-b-2 border-b-transparent duration-500">{{ category.cat_name_bn }}</NuxtLink>
             </li>
         </ul>
     </div>
@@ -20,4 +20,6 @@ headCategory.value = headCat
 
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+    
+</style>
