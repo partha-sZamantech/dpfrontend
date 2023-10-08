400faf63fa02e0838af8a9e58976f44922fb6c26
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-08 13:17:07 +0600

    special content designing

--- components/Home/SpecialTopContent.vue
+++ components/Home/SpecialTopContent.vue
@@ -1,23 +1,82 @@
 <template>
     <div class="specialContent py-3">
-        <div class=" grid md:grid-cols-12 gap-2">
+        <div class=" grid grid-cols-12 gap-2">
             <div class="col-span-12 md:col-span-7">
-                <div class="grid md:grid-cols-12 gap-1">
-                    <div class="col-span-12 md:col-span-7">
-                        <div class="flex flex-col gap-3">
-                            <nuxt-img
-                                src="https://www.dhakaprokash24.com/media/content/images/2023October/nobel-20231005171906.jpg"
-                                class="mx-auto w-full group-hover:scale-110 duration-300"
-                                :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                <div class="grid grid-cols-12 gap-4">
+                    <div class="col-span-12 md:col-span-7 group">
+                        <NuxtLink to="/" class="flex flex-col gap-3">
+                            <div class="overflow-hidden">
+                                <nuxt-img
+                                    src="https://www.dhakaprokash24.com/media/content/images/2023October/nobel-20231005171906.jpg"
+                                    class="mx-auto w-full group-hover:scale-110 duration-300"
+                                    :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                            </div>
                             <h2 class="text-[#ff0000] text-[32px] leading-tight">ইসরাইলের পাশে থাকার ঘোষণা নরেন্দ্র মোদির
                             </h2>
                             <p class="text-[18px]">ফিলিস্তিনি সশস্ত্র গোষ্ঠী হামাসের আকস্মিক হামলায় বিপর্যস্ত হয়ে পড়েছে
                                 ইসরাইল। মাত্র ২০ মিনিটে দেশটিতে পাঁচ হাজার রকেট ছুড়েছে ফিলিস্তিনের স্বাধীনতাকামী সংগঠন
                                 হামাস। এ অবস্থায় ইসরাইলের...</p>
-                        </div>
+                        </NuxtLink>
                     </div>
                     <div class="col-span-12 md:col-span-5">
-
+                        <div class="flex flex-col gap-2">
+                            <NuxtLink to="/" class=" grid grid-cols-12 gap-4 group py-4 border-b specialMiddleTop">
+                                <div class="col-span-12 md:col-span-5">
+                                    <div class="overflow-hidden">
+                                        <nuxt-img
+                                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                            class="mx-auto w-full group-hover:scale-110 duration-300"
+                                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                                    </div>
+                                </div>
+                                <div class="col-span-12 md:col-span-7">
+                                    <h4 class="text-[20px] leading-tight">গাজায় বিদ্যুৎ, জ্বালানি ও পণ্য সরবরাহ বন্ধ করল
+                                        ইসরায়েল</h4>
+                                </div>
+                            </NuxtLink>
+                            <NuxtLink to="/" class=" grid grid-cols-12 gap-4 group py-4 border-b specialMiddleTop">
+                                <div class="col-span-12 md:col-span-5">
+                                    <div class="overflow-hidden">
+                                        <nuxt-img
+                                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                            class="mx-auto w-full group-hover:scale-110 duration-300"
+                                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                                    </div>
+                                </div>
+                                <div class="col-span-12 md:col-span-7">
+                                    <h4 class="text-[20px] leading-tight">গাজায় বিদ্যুৎ, জ্বালানি ও পণ্য সরবরাহ বন্ধ করল
+                                        ইসরায়েল</h4>
+                                </div>
+                            </NuxtLink>
+                            <NuxtLink to="/" class=" grid grid-cols-12 gap-4 group py-4 border-b specialMiddleTop">
+                                <div class="col-span-12 md:col-span-5">
+                                    <div class="overflow-hidden">
+                                        <nuxt-img
+                                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                            class="mx-auto w-full group-hover:scale-110 duration-300"
+                                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                                    </div>
+                                </div>
+                                <div class="col-span-12 md:col-span-7">
+                                    <h4 class="text-[20px] leading-tight">গাজায় বিদ্যুৎ, জ্বালানি ও পণ্য সরবরাহ বন্ধ করল
+                                        ইসরায়েল</h4>
+                                </div>
+                            </NuxtLink>
+                            <NuxtLink to="/" class=" grid grid-cols-12 gap-4 group py-4 border-b specialMiddleTop">
+                                <div class="col-span-12 md:col-span-5">
+                                    <div class="overflow-hidden">
+                                        <nuxt-img
+                                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                            class="mx-auto w-full group-hover:scale-110 duration-300"
+                                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                                    </div>
+                                </div>
+                                <div class="col-span-12 md:col-span-7">
+                                    <h4 class="text-[20px] leading-tight">গাজায় বিদ্যুৎ, জ্বালানি ও পণ্য সরবরাহ বন্ধ করল
+                                        ইসরায়েল</h4>
+                                </div>
+                            </NuxtLink>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -32,4 +91,8 @@
 const img = useImage()
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+    .specialMiddleTop:first-child {
+        padding-top: 0;
+    }
+</style>
