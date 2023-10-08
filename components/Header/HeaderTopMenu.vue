0b1a89d6f43c2b4e17ced79ce8672dd59a0e35de
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-08 16:37:37 +0600

    Header active menu background fixed

--- components/Header/HeaderTopMenu.vue
+++ components/Header/HeaderTopMenu.vue
@@ -1,19 +1,22 @@
 <template>
-    <div :class="`${scrollDown ? 'fixed top-0 right-0 left-0 shadow-lg' : ''} hidden md:block shadow-lg shadow-gray-300 duration-300 bg-[#3375af]`">
+    <div
+        :class="`${scrollDown ? 'fixed top-0 right-0 left-0 shadow-lg' : ''} hidden md:block shadow-lg shadow-gray-300 duration-300 bg-[#3375af]`">
         <ul class="flex gap-0 justify-center text-[18px] text-white">
             <li v-for="category in headCategory" :key="category.cat_id">
-                <NuxtLink :to="`/category/${category.cat_slug}`" class="py-2 px-3 block hover:bg-[#284f81] hover:border-b-2 hover:border-b-white border-b-2 border-b-transparent duration-500">{{ category.cat_name_bn }}</NuxtLink>
+                <NuxtLink :to="`/category/${category.cat_slug}`"
+                    class="py-2 px-3 block hover:bg-[#284f81] hover:border-b-2 hover:border-b-white border-b-2 border-b-transparent duration-500">
+                    {{ category.cat_name_bn }}</NuxtLink>
             </li>
         </ul>
     </div>
 </template>
 
 <script setup>
-const {scrollDown} = defineProps(['scrollDown'])
+const { scrollDown } = defineProps(['scrollDown'])
 
 const headCatconfig = useRuntimeConfig()
 const headCategory = useState(() => [])
-const {data:headCat} = await useFetch(`${headCatconfig.public.apiUrl}/api/headercat`,{
+const { data: headCat } = await useFetch(`${headCatconfig.public.apiUrl}/api/headercat`, {
     method: 'GET'
 })
 headCategory.value = headCat
@@ -21,5 +24,7 @@ headCategory.value = headCat
 </script>
 
 <style scoped>
-    
-</style>
+.router-link-active {
+    background-color: #284f81;
+    border-bottom: 2px solid #fff;
+}</style>
