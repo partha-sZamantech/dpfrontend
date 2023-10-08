a125c987495ddee7dc6133baaea3048c69ce743e
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-08 16:10:55 +0600

    media added

--- components/Home/SpecialTopContent.vue
+++ components/Home/SpecialTopContent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="specialContent py-3">
-        <div class=" grid grid-cols-12 gap-2">
+        <div class=" grid grid-cols-12 gap-5">
             <div class="col-span-12 md:col-span-7">
                 <div class="grid grid-cols-12 gap-4">
                     <div class="col-span-12 md:col-span-7 group">
@@ -30,7 +30,8 @@
                                     </div>
                                 </div>
                                 <div class="col-span-12 md:col-span-7">
-                                    <h4 class="text-[20px] leading-tight">গাজায় বিদ্যুৎ, জ্বালানি ও পণ্য সরবরাহ বন্ধ করল
+                                    <h4 class="text-[20px] leading-tight group-hover:text-[#ff0000]">গাজায় বিদ্যুৎ, জ্বালানি
+                                        ও পণ্য সরবরাহ বন্ধ করল
                                         ইসরায়েল</h4>
                                 </div>
                             </NuxtLink>
@@ -44,7 +45,8 @@
                                     </div>
                                 </div>
                                 <div class="col-span-12 md:col-span-7">
-                                    <h4 class="text-[20px] leading-tight">গাজায় বিদ্যুৎ, জ্বালানি ও পণ্য সরবরাহ বন্ধ করল
+                                    <h4 class="text-[20px] leading-tight group-hover:text-[#ff0000]">গাজায় বিদ্যুৎ, জ্বালানি
+                                        ও পণ্য সরবরাহ বন্ধ করল
                                         ইসরায়েল</h4>
                                 </div>
                             </NuxtLink>
@@ -58,7 +60,8 @@
                                     </div>
                                 </div>
                                 <div class="col-span-12 md:col-span-7">
-                                    <h4 class="text-[20px] leading-tight">গাজায় বিদ্যুৎ, জ্বালানি ও পণ্য সরবরাহ বন্ধ করল
+                                    <h4 class="text-[20px] leading-tight group-hover:text-[#ff0000]">গাজায় বিদ্যুৎ, জ্বালানি
+                                        ও পণ্য সরবরাহ বন্ধ করল
                                         ইসরায়েল</h4>
                                 </div>
                             </NuxtLink>
@@ -72,7 +75,8 @@
                                     </div>
                                 </div>
                                 <div class="col-span-12 md:col-span-7">
-                                    <h4 class="text-[20px] leading-tight">গাজায় বিদ্যুৎ, জ্বালানি ও পণ্য সরবরাহ বন্ধ করল
+                                    <h4 class="text-[20px] leading-tight group-hover:text-[#ff0000]">গাজায় বিদ্যুৎ, জ্বালানি
+                                        ও পণ্য সরবরাহ বন্ধ করল
                                         ইসরায়েল</h4>
                                 </div>
                             </NuxtLink>
@@ -81,7 +85,46 @@
                 </div>
             </div>
             <div class="col-span-12 md:col-span-5">
-                dfjdsf
+                <div class="flex flex-col gap-1">
+                    <div>
+                        <iframe width="518" height="292"
+                            src="https://www.youtube.com/embed/rWdN3xUDdE8?enablejsapi=1&autoplay=1&mute=1&rel=0&showinfo=1&controls=1&loop=1&playlist={{$spTopFirstVideo->code}}"
+                            frameborder="0" allowfullscreen style="width: 100%!important;"></iframe>
+                        <!-- <iframe width="518" height="292" src="https://www.youtube.com/embed/{{$spTopFirstVideo->code}}?enablejsapi=1&autoplay=1&mute=1&rel=0&showinfo=1&controls=1&loop=1&playlist={{$spTopFirstVideo->code}}" frameborder="0" allowfullscreen style="width: 100%!important;"></iframe> -->
+
+                    </div>
+                    <div class="grid grid-cols-2 place-items-center">
+                        <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
+                            <Icon name="simple-icons:youtubemusic" class=" col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                        </NuxtLink>
+                        <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
+                            <Icon name="simple-icons:youtubemusic" class=" col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                        </NuxtLink>
+                        <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
+                            <Icon name="simple-icons:youtubemusic" class=" col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                        </NuxtLink>
+                        <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
+                            <Icon name="simple-icons:youtubemusic" class=" col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                        </NuxtLink>
+                   
+                        <!-- <NuxtLink to="/" class="flex gap-4 items-center group border-b py-4">
+                            <Icon name="simple-icons:youtubemusic" class="text-4xl text-[#ff0000]" />
+                            <h4 class="text-[18px] group-hover:text-[#ff0000]">মা দিবসের পেছনের গল্প</h4>
+                        </NuxtLink>
+                        <NuxtLink to="/" class="flex gap-4 items-center group border-b py-4">
+                            <Icon name="simple-icons:youtubemusic" class="text-4xl text-[#ff0000]" />
+                            <h4 class="text-[18px] group-hover:text-[#ff0000]">মা দিবসের পেছনের গল্প</h4>
+                        </NuxtLink>
+                        <NuxtLink to="/" class="flex gap-4 items-center border-b group py-4">
+                            <Icon name="simple-icons:youtubemusic" class="text-4xl text-[#ff0000]" />
+                            <h4 class="text-[18px] group-hover:text-[#ff0000]">মা দিবসের পেছনের গল্প</h4>
+                        </NuxtLink> -->
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -92,7 +135,7 @@ const img = useImage()
 </script>
 
 <style scoped>
-    .specialMiddleTop:first-child {
-        padding-top: 0;
-    }
+.specialMiddleTop:first-child {
+    padding-top: 0;
+}
 </style>
