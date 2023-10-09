aa8a5e9a5a154c52a6d6d526383cde6ac41251c3
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-09 18:35:37 +0600

    economy and international category home done

--- components/Home/Category/PoliticsEconomyInternational.vue
+++ components/Home/Category/PoliticsEconomyInternational.vue
@@ -8,7 +8,8 @@
                         <h2 class="text-[#3375af] text-[18px] font-semibold">রাজনীতি</h2>
                     </NuxtLink>
                 </div>
-                <div class="home-p-c-ontent">
+                <div class="home-p-c-ontent flex flex-col gap-3">
+                    <!-- Politic Feature Content -->
                     <NuxtLink to="/" class="flex flex-col gap-2 group">
                         <div class=" overflow-hidden">
                             <nuxt-img
@@ -16,8 +17,44 @@
                                 class="mx-auto w-full group-hover:scale-110 duration-300"
                                 :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
                         </div>
-                        <h3 class="text-[19px] leading-tight group-hover:text-[#ff0000]">উন্নত চিকিৎসা না পেলে, যে কোন সময় মারা যেতে পারেন খালেদা জিয়া</h3>
+                        <h3 class="text-[19px] leading-tight group-hover:text-[#ff0000]">উন্নত চিকিৎসা না পেলে, যে কোন সময়
+                            মারা যেতে পারেন খালেদা জিয়া</h3>
                     </NuxtLink>
+                    <!--/ Politic Feature Content -->
+
+                    <div class="h-p-c-excpt flex flex-col">
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -29,6 +66,54 @@
                         <h2 class="text-[#3375af] text-[18px] font-semibold">অর্থনীতি</h2>
                     </NuxtLink>
                 </div>
+                <div class="home-econ-c-ontent flex flex-col gap-3">
+                    <!-- Economy Feature Content -->
+                    <NuxtLink to="/" class="flex flex-col gap-2 group">
+                        <div class=" overflow-hidden">
+                            <nuxt-img
+                                src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                class="mx-auto w-full group-hover:scale-110 duration-300"
+                                :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                        </div>
+                        <h3 class="text-[19px] leading-tight group-hover:text-[#ff0000]">উন্নত চিকিৎসা না পেলে, যে কোন সময়
+                            মারা যেতে পারেন খালেদা জিয়া</h3>
+                    </NuxtLink>
+                    <!--/ Economy Feature Content -->
+
+                    <div class="h-p-c-excpt flex flex-col">
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                    </div>
+
+                </div>
             </div>
         </div>
         <div class=" col-span-12 md:col-span-4">
@@ -38,6 +123,54 @@
                         <span class="w-3 h-3 bg-[#3375af]"></span>
                         <h2 class="text-[#3375af] text-[18px] font-semibold">সারাবিশ্ব</h2>
                     </NuxtLink>
+                </div>
+                <div class="home-int-c-content flex flex-col gap-3">
+                    <!-- International Feature Content -->
+                    <NuxtLink to="/" class="flex flex-col gap-2 group">
+                        <div class=" overflow-hidden">
+                            <nuxt-img
+                                src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                class="mx-auto w-full group-hover:scale-110 duration-300"
+                                :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                        </div>
+                        <h3 class="text-[19px] leading-tight group-hover:text-[#ff0000]">উন্নত চিকিৎসা না পেলে, যে কোন সময়
+                            মারা যেতে পারেন খালেদা জিয়া</h3>
+                    </NuxtLink>
+                    <!--/ International Feature Content -->
+
+                    <div class="h-p-c-excpt flex flex-col">
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                        <!-- Loop Item -->
+                        <NuxtLink to="/" class=" border-b py-3">
+                            <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
+                                প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                        </NuxtLink>
+                        <!--/ Loop Item -->
+                    </div>
+
                 </div>
             </div>
         </div>
