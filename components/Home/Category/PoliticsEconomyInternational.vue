16b0c1db9bc578133aa14d9d3229dda04461b84a
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-09 18:08:47 +0600

    politic done

--- components/Home/Category/PoliticsEconomyInternational.vue
+++ components/Home/Category/PoliticsEconomyInternational.vue
@@ -8,7 +8,17 @@
                         <h2 class="text-[#3375af] text-[18px] font-semibold">রাজনীতি</h2>
                     </NuxtLink>
                 </div>
-             
+                <div class="home-p-c-ontent">
+                    <NuxtLink to="/" class="flex flex-col gap-2 group">
+                        <div class=" overflow-hidden">
+                            <nuxt-img
+                                src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                class="mx-auto w-full group-hover:scale-110 duration-300"
+                                :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                        </div>
+                        <h3 class="text-[19px] leading-tight group-hover:text-[#ff0000]">উন্নত চিকিৎসা না পেলে, যে কোন সময় মারা যেতে পারেন খালেদা জিয়া</h3>
+                    </NuxtLink>
+                </div>
             </div>
         </div>
         <div class=" col-span-12 md:col-span-4">
@@ -35,7 +45,7 @@
 </template>
 
 <script setup>
-
+const img = useImage()
 </script>
 
 <style lang="scss" scoped></style>
