2d6e22629a132301ffb27a8da70bb42d4adf0af1
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-09 17:15:59 +0600

    Home national category feature post

--- components/Home/Category/National.vue
+++ components/Home/Category/National.vue
@@ -1,23 +1,39 @@
 
 <template>
     <div class="home-national-category">
-        <div class="category-header border-b-4 border-b-[#3375af] ">
-            <NuxtLink to="/" class="flex gap-4 items-center">
+        <div class="category-header border-b-4 border-b-[#3375af] my-3">
+            <NuxtLink to="/" class="flex gap-3 items-center">
                 <span class="w-3 h-3 bg-[#3375af]"></span>
                 <h2 class="text-[#3375af] text-[18px] font-semibold">জাতীয়</h2>
-              
             </NuxtLink>
         </div>
-        <div>
-            
+        <div class="grid grid-cols-12 gap-4">
+            <div class="col-span-7">
+                <NuxtLink to="/" class="flex flex-col group gap-2">
+                    <div class="national-feature-image overflow-hidden">
+                        <nuxt-img
+                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                            class="mx-auto w-full group-hover:scale-110 duration-300"
+                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                    </div>
+                    <div class="national-feature-description flex flex-col gap-1">
+                        <h3 class="text-[25px] leading-tight group-hover:text-[#ff0000]">পদ্মা সেতু দিয়ে ছুটবে ট্রেন
+                            দক্ষিণাঞ্চলে, উচ্ছ্বসিত সর্বস্তরের মানুষ</h3>
+                        <p class="text-md">সব জল্পনা-কল্পনার অবসান ঘটিয়ে গত বছরের ২৬ জুন সকাল ৬টা থেকে স্বপ্নের পদ্মা সেতু দিয়ে যান চলাচল শুরু হয়েছে। বাকি ছিল রেল যোগাযোগ। সেই জল্পনা-কল্পনারও অবসান ঘটছে আগামী ১০ অক্টোবর। এর ফলে দক্ষিণ-পশ্চিমাঞ্চলের সঙ্গে গোটা দেশের যোগাযোগ ব্যবস্থায় যোগ হচ্ছে নতুনমাত্রা। দিন যতই এগোচ্ছে তাদের ভেতর আনন্দ আর উদ্দীপনা ততই বাড়ছে।</p>
+                    </div>
+                </NuxtLink>
+            </div>
+            <div class="col-span-5">
+                <div class="">
+
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
-
+const img = useImage()
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
