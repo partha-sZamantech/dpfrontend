80935c637e8fb5629b2274e6d392b28498da1978
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-10 13:48:38 +0600

    Sport category content looping

--- components/Home/Category/Sports.vue
+++ components/Home/Category/Sports.vue
@@ -6,11 +6,111 @@
                 <h2 class="text-[#3375af] text-[18px] font-semibold">খেলা</h2>
             </NuxtLink>
         </div>
+        <div class="grid grid-cols-12 gap-4">
+            <div class="col-span-12 md:col-span-7">
+                <NuxtLink to="/" class="flex flex-col group gap-2">
+                    <div class="national-feature-image overflow-hidden">
+                        <nuxt-img
+                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                            class="mx-auto w-full group-hover:scale-110 duration-300"
+                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                    </div>
+                    <div class="national-feature-description flex flex-col gap-1">
+                        <h3 class="text-[25px] leading-tight group-hover:text-[#ff0000]">পদ্মা সেতু দিয়ে ছুটবে ট্রেন
+                            দক্ষিণাঞ্চলে, উচ্ছ্বসিত সর্বস্তরের মানুষ</h3>
+                        <p class="text-md">সব জল্পনা-কল্পনার অবসান ঘটিয়ে গত বছরের ২৬ জুন সকাল ৬টা থেকে স্বপ্নের পদ্মা সেতু
+                            দিয়ে যান চলাচল শুরু হয়েছে। বাকি ছিল রেল যোগাযোগ। সেই জল্পনা-কল্পনারও অবসান ঘটছে আগামী ১০
+                            অক্টোবর। এর ফলে দক্ষিণ-পশ্চিমাঞ্চলের সঙ্গে গোটা দেশের যোগাযোগ ব্যবস্থায় যোগ হচ্ছে নতুনমাত্রা।
+                            দিন যতই এগোচ্ছে তাদের ভেতর আনন্দ আর উদ্দীপনা ততই বাড়ছে।</p>
+                    </div>
+                </NuxtLink>
+            </div>
+            <div class="col-span-12 md:col-span-5">
+                <div class="home-national-category-except-post flex flex-col">
+                    <!-- Loop Item -->
+                    <div class="grid grid-cols-12 gap-4 group h-sports-excpt border-b py-4">
+                        <div class=" col-span-5 overflow-hidden">
+                            <NuxtLink to="/">
+                                <nuxt-img
+                                    src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                    class="mx-auto w-full group-hover:scale-110 duration-300"
+                                    :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                            </NuxtLink>
+                        </div>
+                        <div class=" col-span-7">
+                            <NuxtLink to="/">
+                                <h4 class="text-[18px] leading-tight group-hover:text-[#ff0000]">শাহজালাল বিমানবন্দরের তৃতীয়
+                                    টার্মিনালের আংশিক উদ্বোধন করলেন প্রধানমন্ত্রী</h4>
+                            </NuxtLink>
+                        </div>
+                    </div>
+                    <!--/ Loop Item -->
+                    <!-- Loop Item -->
+                    <div class="grid grid-cols-12 gap-4 group h-sports-excpt border-b py-5">
+                        <div class=" col-span-5 overflow-hidden">
+                            <NuxtLink to="/">
+                                <nuxt-img
+                                    src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                    class="mx-auto w-full group-hover:scale-110 duration-300"
+                                    :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                            </NuxtLink>
+                        </div>
+                        <div class=" col-span-7">
+                            <NuxtLink to="/">
+                                <h4 class="text-[18px] leading-tight group-hover:text-[#ff0000]">শাহজালাল বিমানবন্দরের তৃতীয়
+                                    টার্মিনালের আংশিক উদ্বোধন করলেন প্রধানমন্ত্রী</h4>
+                            </NuxtLink>
+                        </div>
+                    </div>
+                    <!--/ Loop Item -->
+                    <!-- Loop Item -->
+                    <div class="grid grid-cols-12 gap-4 group h-sports-excpt border-b py-5">
+                        <div class=" col-span-5 overflow-hidden">
+                            <NuxtLink to="/">
+                                <nuxt-img
+                                    src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                    class="mx-auto w-full group-hover:scale-110 duration-300"
+                                    :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                            </NuxtLink>
+                        </div>
+                        <div class=" col-span-7">
+                            <NuxtLink to="/">
+                                <h4 class="text-[18px] leading-tight group-hover:text-[#ff0000]">শাহজালাল বিমানবন্দরের তৃতীয়
+                                    টার্মিনালের আংশিক উদ্বোধন করলেন প্রধানমন্ত্রী</h4>
+                            </NuxtLink>
+                        </div>
+                    </div>
+                    <!--/ Loop Item -->
+                    <!-- Loop Item -->
+                    <div class="grid grid-cols-12 gap-4 group h-sports-excpt border-b py-5">
+                        <div class=" col-span-5 overflow-hidden">
+                            <NuxtLink to="/">
+                                <nuxt-img
+                                    src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                    class="mx-auto w-full group-hover:scale-110 duration-300"
+                                    :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                            </NuxtLink>
+                        </div>
+                        <div class=" col-span-7">
+                            <NuxtLink to="/">
+                                <h4 class="text-[18px] leading-tight group-hover:text-[#ff0000]">শাহজালাল বিমানবন্দরের তৃতীয়
+                                    টার্মিনালের আংশিক উদ্বোধন করলেন প্রধানমন্ত্রী</h4>
+                            </NuxtLink>
+                        </div>
+                    </div>
+                    <!--/ Loop Item -->
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
-
+    const img = useImage()
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+    .h-sports-excpt:first-child{
+        padding-top:0px
+    }
+</style>
