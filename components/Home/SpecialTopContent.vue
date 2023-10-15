259df0074e6f4af03e1cdf4ae90eb4118896a5ad
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-15 13:40:42 +0600

    home page special top data fetching

--- components/Home/SpecialTopContent.vue
+++ components/Home/SpecialTopContent.vue
@@ -4,79 +4,40 @@
             <div class="col-span-12 md:col-span-7">
                 <div class="grid grid-cols-12 gap-4">
                     <div class="col-span-12 md:col-span-7 group">
-                        <NuxtLink :to="`/${specialTopContents[0]?.category?.cat_slug}/${specialTopContents[0]?.content_id}`" class="flex flex-col gap-3" :title="specialTopContents[0]?.content_heading">
+                        <!-- Special First Content -->
+                        <NuxtLink :to="`/${specialTopContents[0]?.category?.cat_slug}/${specialTopContents[0]?.content_id}`"
+                            class="flex flex-col gap-3" :title="specialTopContents[0]?.content_heading">
                             <div class="overflow-hidden">
                                 <nuxt-img
                                     :src="`${config.public.apiUrl}/media/content/images/${specialTopContents[0].img_bg_path}`"
                                     class="mx-auto w-full group-hover:scale-110 duration-300"
-                                    :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" :title="specialTopContents[0]?.content_heading" />
+                                    :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })"
+                                    :title="specialTopContents[0]?.content_heading" :alt="specialTopContents[0]?.content_heading" />
                             </div>
-                            <h2 class="text-[#ff0000] text-[32px] leading-tight">{{ specialTopContents[0]?.content_heading }}</h2>
-                            <p class="text-[18px]">{{ specialTopContents[0]?.content_details.substring(0, 165).toString().replace( /(<([^>]+)>)/ig, '') }}...</p>
+                            <h2 class="text-[#ff0000] text-[32px] leading-tight">{{ specialTopContents[0]?.content_heading
+                            }}</h2>
+                            <p class="text-[18px]">{{ specialTopContents[0]?.content_details.substring(0,
+                                165).toString().replace(/(<([^>]+)>)/ig, '') }}...</p>
                         </NuxtLink>
+                        <!-- Special First Content -->
                     </div>
                     <div class="col-span-12 md:col-span-5">
                         <div class="flex flex-col gap-2">
-                            <NuxtLink to="/" class=" grid grid-cols-12 gap-4 group py-4 border-b specialMiddleTop">
+                            <!-- Loop Item -->
+                            <NuxtLink to="/" class=" grid grid-cols-12 gap-4 group py-4 border-b specialMiddleTop" v-for="topcontent in specialTopContents.slice(1, 5)" :key="topcontent.content_id">
                                 <div class="col-span-5">
                                     <div class="overflow-hidden">
-                                        <nuxt-img
-                                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                                        <nuxt-img :alt="topcontent?.content_heading"
+                                        :src="`${config.public.apiUrl}/media/content/images/${topcontent?.img_bg_path}`"
                                             class="mx-auto w-full group-hover:scale-110 duration-300"
                                             :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
                                     </div>
                                 </div>
                                 <div class="col-span-7">
-                                    <h4 class="text-[20px] leading-tight group-hover:text-[#ff0000]">গাজায় বিদ্যুৎ, জ্বালানি
-                                        ও পণ্য সরবরাহ বন্ধ করল
-                                        ইসরায়েল</h4>
+                                    <h4 class="text-[20px] leading-tight group-hover:text-[#ff0000]">{{ topcontent?.content_heading }}</h4>
                                 </div>
                             </NuxtLink>
-                            <NuxtLink to="/" class=" grid grid-cols-12 gap-4 group py-4 border-b specialMiddleTop">
-                                <div class="col-span-5">
-                                    <div class="overflow-hidden">
-                                        <nuxt-img
-                                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
-                                            class="mx-auto w-full group-hover:scale-110 duration-300"
-                                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
-                                    </div>
-                                </div>
-                                <div class="col-span-7">
-                                    <h4 class="text-[20px] leading-tight group-hover:text-[#ff0000]">গাজায় বিদ্যুৎ, জ্বালানি
-                                        ও পণ্য সরবরাহ বন্ধ করল
-                                        ইসরায়েল</h4>
-                                </div>
-                            </NuxtLink>
-                            <NuxtLink to="/" class=" grid grid-cols-12 gap-4 group py-4 border-b specialMiddleTop">
-                                <div class="col-span-5">
-                                    <div class="overflow-hidden">
-                                        <nuxt-img
-                                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
-                                            class="mx-auto w-full group-hover:scale-110 duration-300"
-                                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
-                                    </div>
-                                </div>
-                                <div class="col-span-7">
-                                    <h4 class="text-[20px] leading-tight group-hover:text-[#ff0000]">গাজায় বিদ্যুৎ, জ্বালানি
-                                        ও পণ্য সরবরাহ বন্ধ করল
-                                        ইসরায়েল</h4>
-                                </div>
-                            </NuxtLink>
-                            <NuxtLink to="/" class=" grid grid-cols-12 gap-4 group py-4 border-b specialMiddleTop">
-                                <div class="col-span-5">
-                                    <div class="overflow-hidden">
-                                        <nuxt-img
-                                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
-                                            class="mx-auto w-full group-hover:scale-110 duration-300"
-                                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
-                                    </div>
-                                </div>
-                                <div class="col-span-7">
-                                    <h4 class="text-[20px] leading-tight group-hover:text-[#ff0000]">গাজায় বিদ্যুৎ, জ্বালানি
-                                        ও পণ্য সরবরাহ বন্ধ করল
-                                        ইসরায়েল</h4>
-                                </div>
-                            </NuxtLink>
+                            <!-- Loop Item -->
                         </div>
                     </div>
                 </div>
@@ -92,22 +53,30 @@
                     </div>
                     <div class="grid grid-cols-1 md:grid-cols-2 md:place-items-center">
                         <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
-                            <Icon name="simple-icons:youtubemusic" class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
-                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                            <Icon name="simple-icons:youtubemusic"
+                                class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের
+                                গল্প</h4>
                         </NuxtLink>
                         <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
-                            <Icon name="simple-icons:youtubemusic" class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
-                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                            <Icon name="simple-icons:youtubemusic"
+                                class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের
+                                গল্প</h4>
                         </NuxtLink>
                         <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
-                            <Icon name="simple-icons:youtubemusic" class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
-                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                            <Icon name="simple-icons:youtubemusic"
+                                class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের
+                                গল্প</h4>
                         </NuxtLink>
                         <NuxtLink to="/" class="grid grid-cols-12 gap-4 items-center group border-b py-4 group">
-                            <Icon name="simple-icons:youtubemusic" class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
-                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের গল্প</h4>
+                            <Icon name="simple-icons:youtubemusic"
+                                class=" col-span-2 md:col-span-3 text-4xl group-hover:text-[#3375af] text-[#ff0000]" />
+                            <h4 class="text-[18px] col-span-10 md:col-span-9 group-hover:text-[#3375af]">মা দিবসের পেছনের
+                                গল্প</h4>
                         </NuxtLink>
-                   
+
                     </div>
                 </div>
             </div>
@@ -127,8 +96,6 @@ specialTopContents.value = spTopCon
 // =============== Special Content Fetching ====================//
 </script>
 
-<style scoped>
-.specialMiddleTop:first-child {
+<style scoped>.specialMiddleTop:first-child {
     padding-top: 0;
-}
-</style>
+}</style>
