638597b370ad19156b83065113637c77ca778e25
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-19 14:40:49 +0600

    Life style content fetching

--- components/Home/Category/Lifestyle.vue
+++ components/Home/Category/Lifestyle.vue
@@ -1,95 +1,50 @@
 <template>
-    <div class="home-entertainment-category">
+    <div class="home-entertainment-category" v-if="lifestyles?.length > 0">
         <div class="category-header border-b-4 border-b-[#3375af] my-3">
-            <NuxtLink to="/" class="flex gap-3 items-center">
+            <NuxtLink :to="`/${lifestyles[0]?.category?.cat_slug}`" class="flex gap-3 items-center">
                 <span class="w-3 h-3 bg-[#3375af]"></span>
                 <h2 class="text-[#3375af] text-[18px] font-semibold">লাইফস্টাইল</h2>
             </NuxtLink>
         </div>
         <div class="home-saradesh-category-except-post flex flex-col">
             <!-- Loop Item -->
-            <div class="grid grid-cols-12 gap-4 group h-sports-excpt border-b py-4">
+            <div class="grid grid-cols-12 gap-4 group h-sports-excpt border-b py-4" v-for="lifestyle in lifestyles"
+                :key="lifestyle.content_id">
                 <div class=" col-span-5 overflow-hidden">
-                    <NuxtLink to="/">
-                        <nuxt-img
-                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                    <NuxtLink :to="`${lifestyle?.category?.cat_slug}/${lifestyle?.content_id}`">
+                        <nuxt-img :src="`${siteurl.site_url}/media/content/images/${lifestyle?.img_bg_path}`"
                             class="mx-auto w-full group-hover:scale-110 duration-300"
-                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                            :placeholder="img(`${siteurl.site_url}/media/common/logo1672518180.png`, { height: 300 })" />
                     </NuxtLink>
                 </div>
                 <div class=" col-span-7">
-                    <NuxtLink to="/">
-                        <h4 class="text-[18px] leading-tight group-hover:text-[#ff0000]">শাহজালাল বিমানবন্দরের তৃতীয়
-                            টার্মিনালের আংশিক উদ্বোধন করলেন প্রধানমন্ত্রী</h4>
+                    <NuxtLink :to="`${lifestyle?.category?.cat_slug}/${lifestyle?.content_id}`">
+                        <h4 class="text-[18px] leading-tight group-hover:text-[#ff0000]">{{
+                                        lifestyle?.content_heading }}</h4>
                     </NuxtLink>
                 </div>
             </div>
             <!--/ Loop Item -->
-            <!-- Loop Item -->
-            <div class="grid grid-cols-12 gap-4 group h-sports-excpt border-b py-5">
-                <div class=" col-span-5 overflow-hidden">
-                    <NuxtLink to="/">
-                        <nuxt-img
-                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
-                            class="mx-auto w-full group-hover:scale-110 duration-300"
-                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
-                    </NuxtLink>
-                </div>
-                <div class=" col-span-7">
-                    <NuxtLink to="/">
-                        <h4 class="text-[18px] leading-tight group-hover:text-[#ff0000]">শাহজালাল বিমানবন্দরের তৃতীয়
-                            টার্মিনালের আংশিক উদ্বোধন করলেন প্রধানমন্ত্রী</h4>
-                    </NuxtLink>
-                </div>
-            </div>
-            <!--/ Loop Item -->
-            <!-- Loop Item -->
-            <div class="grid grid-cols-12 gap-4 group h-sports-excpt border-b py-5">
-                <div class=" col-span-5 overflow-hidden">
-                    <NuxtLink to="/">
-                        <nuxt-img
-                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
-                            class="mx-auto w-full group-hover:scale-110 duration-300"
-                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
-                    </NuxtLink>
-                </div>
-                <div class=" col-span-7">
-                    <NuxtLink to="/">
-                        <h4 class="text-[18px] leading-tight group-hover:text-[#ff0000]">শাহজালাল বিমানবন্দরের তৃতীয়
-                            টার্মিনালের আংশিক উদ্বোধন করলেন প্রধানমন্ত্রী</h4>
-                    </NuxtLink>
-                </div>
-            </div>
-            <!--/ Loop Item -->
-            <!-- Loop Item -->
-            <div class="grid grid-cols-12 gap-4 group h-sports-excpt border-b py-5">
-                <div class=" col-span-5 overflow-hidden">
-                    <NuxtLink to="/">
-                        <nuxt-img
-                            src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
-                            class="mx-auto w-full group-hover:scale-110 duration-300"
-                            :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
-                    </NuxtLink>
-                </div>
-                <div class=" col-span-7">
-                    <NuxtLink to="/">
-                        <h4 class="text-[18px] leading-tight group-hover:text-[#ff0000]">শাহজালাল বিমানবন্দরের তৃতীয়
-                            টার্মিনালের আংশিক উদ্বোধন করলেন প্রধানমন্ত্রী</h4>
-                    </NuxtLink>
-                </div>
-            </div>
-            <!--/ Loop Item -->
+
         </div>
     </div>
 </template>
 
 <script setup>
-    const img = useImage()
+const img = useImage()
+const siteurl = siteUrlState()
+
+// ======== Life Style Content =============== //
+const lifestyles = useState(() => [])
+const { data: lifesc } = await useFetch("/api/home/lifestylecontent", {
+    method: 'GET'
+})
+lifestyles.value = lifesc
+// ======== Life Style Content =============== //
 </script>
 
 <style scoped>
-    .h-sports-excpt:first-child{
-        padding-top:0px
-    }
-
+.h-sports-excpt:first-child {
+    padding-top: 0px
+}
 </style>
