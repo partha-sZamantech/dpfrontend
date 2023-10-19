0ae0e80518ab8fc1f7622f22acdb1b51d68c877b
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-19 18:30:34 +0600

    Probash content fetching from api

--- components/Home/Category/Probash.vue
+++ components/Home/Category/Probash.vue
@@ -1,51 +1,33 @@
 <template>
     <div class="home-probash-category ">
         <div class="category-header border-b-4 border-b-[#3375af] my-3">
-            <NuxtLink to="/" class="flex gap-3 items-center">
+            <NuxtLink :to="`/${probashContents[0]?.category?.cat_slug}`" class="flex gap-3 items-center">
                 <span class="w-3 h-3 bg-[#3375af]"></span>
                 <h2 class="text-[#3375af] text-[18px] font-semibold">প্রবাস</h2>
             </NuxtLink>
         </div>
         <div class="home-int-c-content flex flex-col gap-3">
-            <!-- International Feature Content -->
-            <NuxtLink to="/" class="flex flex-col gap-2 group">
+            <!-- Probash Feature Content -->
+            <NuxtLink :to="`${probashContents[0]?.category?.cat_slug}/${probashContents[0]?.content_id}`"
+                class="flex flex-col gap-2 group">
                 <div class=" overflow-hidden">
-                    <nuxt-img
-                        src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
+                    <nuxt-img :src="`${siteurl.site_url}/media/content/images/${probashContents[0]?.img_bg_path}`"
                         class="mx-auto w-full group-hover:scale-110 duration-300"
-                        :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                        :placeholder="img(`${siteurl.site_url}/media/common/logo1672518180.png`, { height: 300 })" />
                 </div>
-                <h3 class="text-[19px] leading-tight group-hover:text-[#ff0000]">উন্নত চিকিৎসা না পেলে, যে কোন সময়
-                    মারা যেতে পারেন খালেদা জিয়া</h3>
+                <h3 class="text-[19px] leading-tight group-hover:text-[#ff0000]">{{ probashContents[0]?.content_heading }}
+                </h3>
             </NuxtLink>
-            <!--/ International Feature Content -->
+            <!--/ Probash Feature Content -->
 
             <div class="h-p-c-excpt flex flex-col">
                 <!-- Loop Item -->
-                <NuxtLink to="/" class=" border-b py-3">
-                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
-                        প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                <NuxtLink :to="`${probashContent?.category?.cat_slug}/${probashContent?.content_id}`"
+                    class=" border-b py-3" v-for="probashContent in probashContents.slice(1, 5)"
+                    :key="probashContent.content_id">
+                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">{{ probashContent?.content_heading }}</h4>
                 </NuxtLink>
                 <!--/ Loop Item -->
-                <!-- Loop Item -->
-                <NuxtLink to="/" class=" border-b py-3">
-                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
-                        প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
-                </NuxtLink>
-                <!--/ Loop Item -->
-                <!-- Loop Item -->
-                <NuxtLink to="/" class=" border-b py-3">
-                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
-                        প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
-                </NuxtLink>
-                <!--/ Loop Item -->
-                <!-- Loop Item -->
-                <NuxtLink to="/" class=" border-b py-3">
-                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
-                        প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
-                </NuxtLink>
-                <!--/ Loop Item -->
-      
             </div>
 
         </div>
@@ -54,6 +36,15 @@
 
 <script setup>
 const img = useImage()
+const siteurl = siteUrlState()
+
+// ======== Probash Content =============== //
+const probashContents = useState(() => [])
+const { data: hprobash } = await useFetch("/api/home/probash", {
+    method: 'GET'
+})
+probashContents.value = hprobash
+// ======== Probash Content =============== //
 </script>
 
 <style lang="scss" scoped></style>
