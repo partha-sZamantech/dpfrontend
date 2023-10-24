14ef924a1a2c894455a6f5851ef0d7a94030080a
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-24 13:53:29 +0600

    Opinion content added

--- components/Home/Category/Opinion.vue
+++ components/Home/Category/Opinion.vue
@@ -1,68 +1,30 @@
 <template>
     <div class="home-opinion-category ">
         <div class="category-header border-b-4 border-b-[#3375af] my-3">
-            <NuxtLink to="/" class="flex gap-3 items-center">
+            <NuxtLink :to="`/${opinionContents[0]?.category?.cat_slug}`" class="flex gap-3 items-center">
                 <span class="w-3 h-3 bg-[#3375af]"></span>
                 <h2 class="text-[#3375af] text-[18px] font-semibold">মতামত</h2>
             </NuxtLink>
         </div>
         <div class="home-int-c-content flex flex-col gap-3">
-            <!-- International Feature Content -->
-            <NuxtLink to="/" class="flex flex-col gap-2 group">
+            <!-- Opinion Feature Content -->
+            <NuxtLink :to="`${opinionContents[0]?.category?.cat_slug}/${opinionContents[0]?.content_id}`" class="flex flex-col gap-2 group">
                 <div class=" overflow-hidden">
-                    <nuxt-img
-                        src="https://www.dhakaprokash24.com/media/content/images/2023October/SM/gaja-2-20231008122635.jpg"
-                        class="mx-auto w-full group-hover:scale-110 duration-300"
-                        :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                    <nuxt-img :src="`${siteurl.site_url}/media/content/images/${opinionContents[0]?.img_bg_path}`"
+                            class="mx-auto w-full group-hover:scale-110 duration-300"
+                            :placeholder="img(`${siteurl.site_url}/media/common/logo1672518180.png`, { height: 300 })" />
                 </div>
-                <h3 class="text-[19px] leading-tight group-hover:text-[#ff0000]">উন্নত চিকিৎসা না পেলে, যে কোন সময়
-                    মারা যেতে পারেন খালেদা জিয়া</h3>
+                <h3 class="text-[19px] leading-tight group-hover:text-[#ff0000]">{{ opinionContents[0]?.content_heading }}</h3>
             </NuxtLink>
-            <!--/ International Feature Content -->
+            <!--/ Opinion Feature Content -->
 
             <div class="h-p-c-excpt flex flex-col">
                 <!-- Loop Item -->
-                <NuxtLink to="/" class=" border-b py-3">
-                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
-                        প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
+                <NuxtLink :to="`${opinionContent?.category?.cat_slug}/${opinionContent?.content_id}`" class=" border-b py-3" v-for="opinionContent in opinionContents.slice(1, 7)" :key="opinionContent.content_id">
+                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">{{ opinionContent?.content_heading }}</h4>
                 </NuxtLink>
                 <!--/ Loop Item -->
-                <!-- Loop Item -->
-                <NuxtLink to="/" class=" border-b py-3">
-                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
-                        প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
-                </NuxtLink>
-                <!--/ Loop Item -->
-                <!-- Loop Item -->
-                <NuxtLink to="/" class=" border-b py-3">
-                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
-                        প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
-                </NuxtLink>
-                <!--/ Loop Item -->
-                <!-- Loop Item -->
-                <NuxtLink to="/" class=" border-b py-3">
-                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
-                        প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
-                </NuxtLink>
-                <!--/ Loop Item -->
-                <!-- Loop Item -->
-                <NuxtLink to="/" class=" border-b py-3">
-                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
-                        প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
-                </NuxtLink>
-                <!--/ Loop Item -->
-                <!-- Loop Item -->
-                <NuxtLink to="/" class=" border-b py-3">
-                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
-                        প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
-                </NuxtLink>
-                <!--/ Loop Item -->
-                <!-- Loop Item -->
-                <NuxtLink to="/" class=" border-b py-3">
-                    <h4 class="text-[17px] hover:text-[#ff0000] leading-tight">ভিসানীতি নিয়ে বেশি আতঙ্কে
-                        প্রধানমন্ত্রী: মির্জা ফখরুল</h4>
-                </NuxtLink>
-                <!--/ Loop Item -->
+
             </div>
 
         </div>
@@ -71,6 +33,15 @@
 
 <script setup>
 const img = useImage()
+const siteurl = siteUrlState()
+
+// ======== Opinion Content =============== //
+const opinionContents = useState(() => [])
+const { data: hmOpinion } = await useFetch("/api/home/opinioncontent", {
+    method: 'GET'
+})
+opinionContents.value = hmOpinion
+// ======== Opinion Content =============== //
 </script>
 
 <style lang="scss" scoped></style>
