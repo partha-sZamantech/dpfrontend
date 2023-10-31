7d88a1c27156c3ab15e804e94577acc109ce0d4c
Parthadeb Mondal <Parthadev76@gmail.com>
2023-10-31 18:30:17 +0600

    Dhaka proaksh detail page added

--- pages/category/[category_slug]/[content_id].vue
+++ pages/category/[category_slug]/[content_id].vue
@@ -15,8 +15,44 @@
         </div>
         <div class=" grid grid-cols-12 gap-4">
             <div class=" col-span-9">
-                <div class="single-post">
-                    <h2 class="text-[40px] leading-[50px]">‘বাইডেন সাহেব ট্রাম্পের সঙ্গে যেদিন ডায়ালগ করবেন, সেদিন আমিও করব’</h2>
+                <div class="single-post flex flex-col gap-3">
+                    <div class="singlePost-heading flex flex-col gap-2">
+                        <h2 class="md:text-[32px] md:leading-[50px]">‘বাইডেন সাহেব ট্রাম্পের সঙ্গে যেদিন ডায়ালগ করবেন, সেদিন
+                            আমিও করব’</h2>
+                        <div class="h-2 w-12 rounded-md bg-[#3375af]"></div>
+                    </div>
+
+                    <div class="flex justify-between items-end">
+                        <div class="author-details flex flex-col gap-1">
+                            <p>মুন্সীগঞ্জ প্রতিনিধি</p>
+                            <p>প্রকাশ: <span>৩১ অক্টোবর ২০২৩, ০৫:২৮ পিএম</span></p>
+                        </div>
+                        <div class="social-item flex gap-2 items-start justify-center">
+                            <NuxtLink to="/">
+                                <svg class=" hover:scale-125 duration-200" xmlns="http://www.w3.org/2000/svg" height="28" width="28" viewBox="0 0 32 32"
+                                    enable-background="new 0 0 32 32" xml:space="preserve">
+                                    <path fill="#1877F2"
+                                        d="M16,0L16,0c8.837,0,16,7.163,16,16l0,0c0,8.837-7.163,16-16,16l0,0C7.163,32,0,24.837,0,16l0,0 C0,7.163,7.163,0,16,0z">
+                                    </path>
+                                    <path fill="#FFFFFF"
+                                        d="M18,17.5h2.5l1-4H18v-2c0-1.03,0-2,2-2h1.5V6.14C21.174,6.097,19.943,6,18.643,6C15.928,6,14,7.657,14,10.7 v2.8h-3v4h3V26h4V17.5z">
+                                    </path>
+                                </svg>
+                            </NuxtLink>
+                            <NuxtLink to="/">
+                                <svg class=" hover:scale-125 duration-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                    enable-background="new 0 0 24 24" xml:space="preserve" height="28" width="28">
+                                    <circle fill="#0F1419" cx="12" cy="12" r="12"></circle>
+                                    <path fill="#FFFFFF" d="M15.531,7h1.662l-3.63,4.236L17.833,17h-3.343l-2.62-3.495L8.876,17H7.212l3.882-4.531L7,7h3.427
+l2.366,3.195L15.531,7z M14.947,15.986h0.92L9.926,7.962H8.937L14.947,15.986z"></path>
+                                </svg>
+                            </NuxtLink>
+                            <NuxtLink to="/">
+                                <svg class=" hover:scale-125 duration-200" height="28" width="28" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" enable-background="new 0 0 32 32" xml:space="preserve"><path fill="#595959" d="M16,0L16,0c8.837,0,16,7.163,16,16l0,0c0,8.837-7.163,16-16,16l0,0C7.163,32,0,24.837,0,16l0,0 C0,7.163,7.163,0,16,0z"></path><path fill="#FFFFFF" d="M12,20h8v4h-8V20z M21.6,22.4v-4H10.4v4H8.8c-0.212,0-0.416-0.084-0.566-0.234C8.084,22.016,8,21.812,8,21.6 v-8c0-0.212,0.084-0.416,0.234-0.566C8.384,12.884,8.588,12.8,8.8,12.8h14.4c0.212,0,0.416,0.084,0.566,0.234 C23.916,13.184,24,13.388,24,13.6v8c0,0.212-0.084,0.416-0.234,0.566c-0.15,0.15-0.353,0.234-0.566,0.234H21.6z M10.4,14.4V16h2.4 v-1.6H10.4z M12,8h8c0.212,0,0.416,0.084,0.566,0.234C20.716,8.384,20.8,8.588,20.8,8.8v2.4h-9.6V8.8 c0-0.212,0.084-0.416,0.234-0.566C11.584,8.084,11.788,8,12,8z"></path></svg>
+                            </NuxtLink>
+                        </div>
+                    </div>
+
                 </div>
             </div>
             <div class=" col-span-3">
@@ -83,12 +119,11 @@
                         </div>
                         <!--/ Loop Item -->
 
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-</template>
+</div></template>
 
 <script setup>
 const img = useImage()
