73ea6470d6685aea2e7db9a4b20103a5766673a1
Parthadeb Mondal <Parthadev76@gmail.com>
2023-11-09 17:56:26 +0600

    Space for ads

--- pages/[category]/index.vue
+++ pages/[category]/index.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        category page
+    <div class="category-page">
+        <!-- Space For Ads -->
+        <div class="category-ads-section border-b py-4 ">
+            <div class="bg-[#f7f7f7]">
+                <img class="mx-auto" src="/assets/img/bar-ads.gif" alt="">
+            </div>
+        </div>
+        <!-- Space For Ads -->
     </div>
 </template>
 
