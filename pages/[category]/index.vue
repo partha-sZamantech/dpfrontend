71a2708eeee7199bf7485e14a5b2a97f55997926
Parthadeb Mondal <Parthadev76@gmail.com>
2023-11-09 18:56:07 +0600

    category page desigin

--- pages/[category]/index.vue
+++ pages/[category]/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="category-page">
+
         <Head>
             <Title>{{ category.cat_name_bn }}</Title>
         </Head>
@@ -13,23 +14,23 @@
         </div>
         <!-- Space For Ads -->
         <div class=" max-w-[1280px] mx-auto category-content px-4 md:px-2 py-4">
-            <div class="breadcrump border-b pb-1 mb-5">
+            <div class="breadcrump border-b pb-1 mb-5 flex flex-col gap-2">
                 <div class="flex gap-1 justify-start items-center">
 
                     <NuxtLink to="/">
                         <Icon class="text-xl" name="material-symbols:house-rounded" />
                     </NuxtLink>
-                    <Icon name="ic:outline-keyboard-arrow-right" />
-                    <NuxtLink :to="`/`">
+                    <Icon name="ic:outline-keyboard-arrow-right" class="text-[#3375af]" />
+                    <NuxtLink :to="`/`" class="text-[#3375af] font-semibold">
                         <!-- {{ detailsContent?.category?.cat_name_bn }} -->
                         {{ category.cat_name_bn }}
                     </NuxtLink>
-
-                    <!-- <Icon v-if="detailsContent?.subcategory" name="ic:outline-keyboard-arrow-right" />
-                    <NuxtLink v-if="detailsContent?.subcategory" :to="`/`">
-                        {{ detailsContent?.subcategory?.subcat_name_bn }}
-                    </NuxtLink> -->
-
+                </div>
+                <div class="subcategory flex gap-4" v-if="category?.subcat?.length > 0">
+                    <!-- <Icon v-if="detailsContent?.subcategory" name="ic:outline-keyboard-arrow-right" /> -->
+                    <NuxtLink v-for="subcategory in category?.subcat" :to="`/`" class="text-[000000] font-[600]">
+                        {{ subcategory.subcat_name_bn }}
+                    </NuxtLink>
                 </div>
             </div>
         </div>
@@ -40,18 +41,18 @@
 const cat_slug = useRoute().params.category
 const category = ref('')
 //================== Category Content fetching =============== //
-    const categoryContent = useState(() => [])
-    const take = ref(15)
-    const {data:catcont} = await useFetch('/api/category/categorycontent', {
-        method: "POST", 
-        body: {
-            cat_slug : cat_slug,
-            take: take.value
-        }
-    })
-    categoryContent.value = catcont.value
-    category.value = catcont?.value?.category
-    console.log(categoryContent.value)
+const categoryContent = useState(() => [])
+const take = ref(15)
+const { data: catcont } = await useFetch('/api/category/categorycontent', {
+    method: "POST",
+    body: {
+        cat_slug: cat_slug,
+        take: take.value
+    }
+})
+categoryContent.value = catcont.value
+category.value = catcont?.value?.category
+console.log(categoryContent.value)
 //================== Category Content fetching =============== //
 
 </script>
