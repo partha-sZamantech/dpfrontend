488c10bc4e939c95c39ea84b077765d537d81f30
Parthadeb Mondal <Parthadev76@gmail.com>
2023-11-11 12:49:53 +0600

    Category page leadpost

--- pages/[category]/index.vue
+++ pages/[category]/index.vue
@@ -28,13 +28,38 @@
                 </div>
                 <div>
                     <div class="subcategory flex flex-wrap gap-3" v-if="category?.subcat?.length > 0">
-                    <!-- <Icon v-if="detailsContent?.subcategory" name="ic:outline-keyboard-arrow-right" /> -->
-                    <div class="subcategoryLink" v-for="subcategory in category?.subcat">
-                        <NuxtLink  :to="`/`" class="text-[000000] font-[600] hover:text-[#3375af]">
-                        {{ subcategory.subcat_name_bn }}
-                    </NuxtLink>
+                        <!-- <Icon v-if="detailsContent?.subcategory" name="ic:outline-keyboard-arrow-right" /> -->
+                        <div class="subcategoryLink" v-for="subcategory in category?.subcat">
+                            <NuxtLink :to="`/`" class="text-[000000] font-[600] hover:text-[#3375af]">
+                                {{ subcategory.subcat_name_bn }}
+                            </NuxtLink>
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div class="grid grid-cols-12">
+                <div class="col-span-9">
+                    <div class="grid grid-cols-12">
+                        <div class="col-span-8">
+                            <div class="lead-post h-[328px]">
+                                <NuxtLink :to="`/`" class=" relative">
+                                    <nuxt-img
+                                        :src="`https://www.dhakaprokash24.com/media/content/images/2023October/asad-dhakaprokash-20231005140541.jpg`"
+                                        class="mx-auto w-full group-hover:scale-110 duration-300 h-full"
+                                        :placeholder="img('https://www.dhakaprokash24.com/media/common/logo1672518180.png', { height: 300 })" />
+                                    <div class="lead-overly absolute h-full w-full block top-0">
+                                        <h5 class="img-title text-center text-white group-hover:text-[#ff0000] text-[19px] absolute bottom-4 left-6">
+                                            dfdsf</h5>
+                                    </div>
+                                </NuxtLink>
+                            </div>
+                        </div>
+                        <div class="col-span-4">
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-span-3">
+                    dfds
                 </div>
             </div>
         </div>
@@ -42,6 +67,9 @@
 </template>
 
 <script setup>
+
+const img = useImage()
+const siteurl = siteUrlState()
 const cat_slug = useRoute().params.category
 const category = ref('')
 //================== Category Content fetching =============== //
@@ -71,5 +99,8 @@ console.log(categoryContent.value)
     border-radius: 100%;
     margin-bottom: 3px;
     margin-left: 10px;
+}
+.lead-overly{
+    background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(53, 50, 50, 0.9) 75%, rgb(0, 0, 0) 100%);
 }
 </style>
