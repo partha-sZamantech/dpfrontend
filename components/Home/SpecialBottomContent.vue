053fad1ee04f75c7432e6e869e84b8ad3f9f1321
Parthadeb Mondal <Parthadev76@gmail.com>
2023-11-13 15:46:13 +0600

    Special bottom content placeholder image added

--- components/Home/SpecialBottomContent.vue
+++ components/Home/SpecialBottomContent.vue
@@ -2,11 +2,11 @@
     <div class="grid grid-cols-2 md:grid-cols-3 gap-5 py-4">
         <ClientOnly>
         <!-- ========== Loop Item =========== -->
-        <NuxtLink :to="`/category/${tpcontent?.category?.cat_slug}/${tpcontent?.content_id}`" class="flex flex-col gap-3 group" v-for="tpcontent in spTopContent.slice(5, 11)" :key="tpcontent.content_id">
+        <NuxtLink :to="`/category/${tpcontent?.category?.cat_slug}/${tpcontent?.content_id}`" class="flex flex-col gap-3 group" v-for="tpcontent in spTopContent?.slice(5, 11)" :key="tpcontent?.content_id">
             <div class=" overflow-hidden">
-                <nuxt-img :src="`${siteurl.site_url}/media/content/images/${tpcontent?.img_bg_path}`"
+                <nuxt-img :src="`${siteurl?.site_url}/media/content/images/${tpcontent?.img_bg_path}`"
                     class="mx-auto w-full group-hover:scale-110 duration-300"
-                    :placeholder="img(`${siteurl.site_url}/media/common/logo1672518180.png`, { height: 300 })" />
+                    :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)" />
             </div>
             <div>
                 <h4 class="leading-tight text-[20px] mb-1 group-hover:text-[#ff0000]">{{ tpcontent?.content_heading }}</h4>
