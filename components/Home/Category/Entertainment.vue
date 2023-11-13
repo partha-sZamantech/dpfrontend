3affef5b23a473b522fb22b9ef07fba6f6f51bfa
Parthadeb Mondal <Parthadev76@gmail.com>
2023-11-13 15:57:25 +0600

    Entertainment placeholder added

--- components/Home/Category/Entertainment.vue
+++ components/Home/Category/Entertainment.vue
@@ -31,7 +31,7 @@
                             <NuxtLink :to="`/category/${entertainment?.category?.cat_slug}/${entertainment?.content_id}`">
                                 <nuxt-img :src="`${siteurl.site_url}/media/content/images/${entertainment?.img_bg_path}`"
                                 class="mx-auto w-full group-hover:scale-110 duration-300"
-                                :placeholder="img(`${siteurl.site_url}/media/common/logo1672518180.png`, { height: 300 })" />
+                                :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)"  />
                             </NuxtLink>
                         </div>
                         <div class=" col-span-7">
