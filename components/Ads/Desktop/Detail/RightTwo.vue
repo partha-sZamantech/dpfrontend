6691f9d6e9d68e85a8320c5b57798acde9ee9d60
Parthadeb Mondal <Parthadev76@gmail.com>
2023-11-28 13:52:53 +0600

    Detail page right two ads fixed component code

---
+++ components/Ads/Desktop/Detail/RightTwo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="data?.DetailRightTwoAds?.status === 1" class="ad-container">
+        <div class="ad-section bg-[#f7f7f7]">
+            <a :href="data?.DetailRightTwoAds?.external_link" target="_blank" rel="nofollow" v-if="data?.DetailRightTwoAds?.type === 3">
+                <img class="mx-auto" :src="`${siteurl.site_url}/media/advertisement/${data?.DetailRightTwoAds?.desktop_image_path}`"
+                    alt="Header Ad" />
+            </a>
+            <div v-else v-html="data?.DetailRightTwoAds?.code"></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const siteurl = siteUrlState()
+const data = defineProps(['DetailRightTwoAds'])
+
+</script>
+
+<style lang="scss" scoped></style>
