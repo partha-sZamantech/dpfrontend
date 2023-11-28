43139bfb9850ccb0b3d4e3eef9e19ff935c432c3
Parthadeb Mondal <Parthadev76@gmail.com>
2023-11-28 13:48:42 +0600

    Detail page bottom ads component code

---
+++ components/Ads/Desktop/Detail/Bottom.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="data?.DetailBottomAds?.status === 1" class="ad-container">
+        <div class="ad-section bg-[#f7f7f7]">
+            <a :href="data?.DetailBottomAds?.external_link" target="_blank" rel="nofollow" v-if="data?.DetailBottomAds?.type === 3">
+                <img class="mx-auto" :src="`${siteurl.site_url}/media/advertisement/${data?.DetailBottomAds?.desktop_image_path}`"
+                    alt="Header Ad" />
+            </a>
+            <div v-else v-html="data?.DetailBottomAds?.code"></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const siteurl = siteUrlState()
+const data = defineProps(['DetailBottomAds'])
+
+</script>
+
+<style lang="scss" scoped></style>
