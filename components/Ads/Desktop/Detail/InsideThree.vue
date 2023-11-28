01ea69eee78047172e608979f0275e451952d937
Parthadeb Mondal <Parthadev76@gmail.com>
2023-11-28 13:49:56 +0600

    Detail page inside three ads component code

---
+++ components/Ads/Desktop/Detail/InsideThree.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="data?.DetailInsideThreeAds?.status === 1" class="ad-container">
+        <div class="ad-section bg-[#f7f7f7]">
+            <a :href="data?.DetailInsideOneAds?.external_link" target="_blank" rel="nofollow" v-if="data?.DetailInsideThreeAds?.type === 3">
+                <img class="mx-auto" :src="`${siteurl.site_url}/media/advertisement/${data?.DetailInsideThreeAds?.desktop_image_path}`"
+                    alt="Header Ad" />
+            </a>
+            <div v-else v-html="data?.DetailInsideThreeAds?.code"></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const siteurl = siteUrlState()
+const data = defineProps(['DetailInsideThreeAds'])
+
+</script>
+
+<style lang="scss" scoped></style>
