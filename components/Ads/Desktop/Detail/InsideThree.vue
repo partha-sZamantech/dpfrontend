ac6f39818903a2d1d71e662af93ab4032910c759
Parthadeb Mondal <Parthadev76@gmail.com>
2023-11-28 13:50:23 +0600

    Detail page inside three ads fixed component code

--- components/Ads/Desktop/Detail/InsideThree.vue
+++ components/Ads/Desktop/Detail/InsideThree.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="data?.DetailInsideThreeAds?.status === 1" class="ad-container">
         <div class="ad-section bg-[#f7f7f7]">
-            <a :href="data?.DetailInsideOneAds?.external_link" target="_blank" rel="nofollow" v-if="data?.DetailInsideThreeAds?.type === 3">
+            <a :href="data?.DetailInsideThreeAds?.external_link" target="_blank" rel="nofollow" v-if="data?.DetailInsideThreeAds?.type === 3">
                 <img class="mx-auto" :src="`${siteurl.site_url}/media/advertisement/${data?.DetailInsideThreeAds?.desktop_image_path}`"
                     alt="Header Ad" />
             </a>
