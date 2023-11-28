a40513f1d949a56c2037270f5fe190fc67fe9c51
Parthadeb Mondal <Parthadev76@gmail.com>
2023-11-28 13:50:53 +0600

    Detail page inside two  ads fixed component code

---
+++ components/Ads/Desktop/Detail/InsideTwo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="data?.DetailInsideTwoAds?.status === 1" class="ad-container">
+        <div class="ad-section bg-[#f7f7f7]">
+            <a :href="data?.DetailInsideTwoAds?.external_link" target="_blank" rel="nofollow" v-if="data?.DetailInsideTwoAds?.type === 3">
+                <img class="mx-auto" :src="`${siteurl.site_url}/media/advertisement/${data?.DetailInsideTwoAds?.desktop_image_path}`"
+                    alt="Header Ad" />
+            </a>
+            <div v-else v-html="data?.DetailInsideTwoAds?.code"></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const siteurl = siteUrlState()
+const data = defineProps(['DetailInsideTwoAds'])
+
+</script>
+
+<style lang="scss" scoped></style>
