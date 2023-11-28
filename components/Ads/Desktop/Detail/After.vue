0eafae2905da48cdd4a056e4956182f3ae5d78ad
Parthadeb Mondal <Parthadev76@gmail.com>
2023-11-28 13:48:05 +0600

    Detail page After ads component code

---
+++ components/Ads/Desktop/Detail/After.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="data?.DetailAfterAds?.status === 1" class="ad-container">
+        <div class="ad-section bg-[#f7f7f7]">
+            <a :href="data?.DetailAfterAds?.external_link" target="_blank" rel="nofollow" v-if="data?.DetailAfterAds?.type === 3">
+                <img class="mx-auto" :src="`${siteurl.site_url}/media/advertisement/${data?.DetailAfterAds?.desktop_image_path}`"
+                    alt="Header Ad" />
+            </a>
+            <div v-else v-html="data?.DetailAfterAds?.code"></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const siteurl = siteUrlState()
+const data = defineProps(['DetailAfterAds'])
+
+</script>
+
+<style lang="scss" scoped></style>
