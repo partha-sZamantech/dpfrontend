3ecc05e14ca705bd5de823f327c2952c4825034e
Parthadeb Mondal <Parthadev76@gmail.com>
2023-12-10 12:26:38 +0600

    Dhaka prokash 24

---
+++ server/api/detailpage/single/getmorecontents.post.ts
@@ -0,0 +1,14 @@
+export default defineEventHandler( async (event) => {
+    
+    const config = useRuntimeConfig()
+    const getBody = await readBody(event)
+
+    const data = await $fetch(`${config.public.apiUrl}/api/detgetmorecontent/${getBody?.cat_id}/${getBody?.content_id}`, {
+        method: 'get'
+    })
+
+
+    return data;
+    
+
+})
