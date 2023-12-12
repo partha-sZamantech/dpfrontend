114782dfd29f0a18c0115c27aba009a7b9eeddec
Parthadeb Mondal <Parthadev76@gmail.com>
2023-12-12 14:11:44 +0600

    Home video api generate

---
+++ server/api/prismaapi/home/videofeature.post.ts
@@ -0,0 +1,67 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+export default defineEventHandler(async (event) => {
+
+    const prisma = new PrismaClient()
+    const getBody = await readBody(event)
+
+    // Get Video Position
+    const videoPosition = await prisma.bn_video_positions.findFirst({
+        where: {
+            position_id: 1,
+            deletable: 1
+        },
+
+    })
+
+    const data = []
+    if (videoPosition && videoPosition?.video_ids?.length > 0) {
+        const vdieoPositionArray = videoPosition?.video_ids?.split(',')
+        const getVideoIds = vdieoPositionArray?.splice(0, parseInt(getBody?.take))
+
+        for (let i = 0; i < getVideoIds?.length; i++) {
+            const video = await prisma.bn_videos.findFirst({
+                where: {
+                    id: parseInt(getVideoIds[i]),
+                    status: 1,
+                    deletable: 1
+                }
+            })
+
+
+            // Category
+            const category = await prisma.bn_video_categories.findFirst({
+                where: {
+                    id: parseInt(video?.cat_id)
+                },
+                select: {
+                    id: true,
+                    slug: true,
+                    name_bn: true
+                }
+            })
+
+            data.push({
+                id: video?.id,
+                cat_id: category?.id,
+                cat_slug: category?.slug,
+                type: video?.type,
+                title: video?.title,
+                img_bg_path: video?.img_bg_path,
+                img_sm_path: video?.img_sm_path,
+                code: video?.code,
+                is_live: video?.is_live,
+                target: video?.target
+            })
+          
+
+        }
+
+        return data
+
+    }
+
+
+
+
+
+})
