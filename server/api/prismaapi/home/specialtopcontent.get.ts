a689bfc700357f731671548bbdbe817f1fa7dfb7
Parthadeb Mondal <Parthadev76@gmail.com>
2023-12-12 12:23:44 +0600

    Special top content api generate

--- server/api/prismaapi/home/specialtopcontent.get.ts
+++ server/api/prismaapi/home/specialtopcontent.get.ts
@@ -3,27 +3,26 @@ export default defineEventHandler(async (event) => {
 
     // const getBody = await readBody(event)
     const prisma = new PrismaClient()
-    const getBody = await readBody(event)
+    // const getBody = await readBody(event)
     const position = await prisma.bn_content_positions.findFirst({
         where: {
-            cat_id: parseInt(getBody?.cat_id),
+            position_id: 1,
             status: 1,
             deletable: 1
         },
         select: {
-            cat_id: true,
             position_id: true,
-            position_name: true,
+            cat_id: true,
             special_cat_id: true,
             subcat_id: true,
-            content_ids: true,
-            total_content: true
+            content_ids: true
         }
     })
+
     const data = []
-    if (position && position?.content_ids?.length > 0) {
+    if(position && position?.content_ids?.length > 0){
         const positionArray = position?.content_ids?.split(',')
-        const getContents = positionArray?.splice(0, getBody?.take)
+        const getContents = positionArray?.splice(0, 11)
         for (let i = 0; i < getContents?.length; i++) {
             const content = await prisma.bn_contents.findFirst({
                 where: {
@@ -39,7 +38,7 @@ export default defineEventHandler(async (event) => {
             })
             const subcategory = await prisma.bn_subcategories.findFirst({
                 where: {
-                    cat_id: parseInt(category?.cat_id)
+                    cat_id: parseInt(content?.cat_id)
                 }
             })
             data.push({
@@ -54,8 +53,5 @@ export default defineEventHandler(async (event) => {
         }
 
         return data
-
     }
-
-
 })
