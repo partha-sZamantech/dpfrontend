c0bbc344ae6d26b0a8af67054b0ccd95ad5db136
Parthadeb Mondal <Parthadev76@gmail.com>
2023-12-12 15:11:42 +0600

    SPort api generate

--- components/Home/Category/Sports.vue
+++ components/Home/Category/Sports.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="home-sports-category">
         <div class="category-header border-b-4 border-b-[#3375af] my-3">
-            <NuxtLink :to="`/${sportscontent[0]?.category?.cat_slug}`" class="flex gap-3 items-center">
+            <NuxtLink :to="`/${sportscontent[0]?.cat_slug}`" class="flex gap-3 items-center">
                 <span class="w-3 h-3 bg-[#3375af]"></span>
                 <h2 class="text-[#3375af] text-[18px] font-semibold">খেলা</h2>
             </NuxtLink>
         </div>
         <div class="grid grid-cols-12 gap-4">
             <div class="col-span-12 md:col-span-7">
-                <NuxtLink :to="`/category/${sportscontent[0]?.category?.cat_slug}/${sportscontent[0]?.content_id}`"
+                <NuxtLink :to="`/category/${sportscontent[0]?.cat_slug}/${sportscontent[0]?.content_id}`"
                     class="flex flex-col group gap-2">
                     <div class="national-feature-image overflow-hidden">
                         <nuxt-img :src="`${siteUrl.site_url}/media/content/images/${sportscontent[0]?.img_bg_path}`"
@@ -33,14 +33,14 @@
                     <div class="grid grid-cols-12 gap-4 group h-sports-excpt border-b py-4"
                         v-for="hmsport in sportscontent.slice(1, 5)" :key="hmsport.content_id">
                         <div class=" col-span-5 overflow-hidden">
-                            <NuxtLink :to="`/category/${hmsport?.category?.cat_slug}/${hmsport?.content_id}`">
+                            <NuxtLink :to="`/category/${hmsport?.cat_slug}/${hmsport?.content_id}`">
                                 <nuxt-img :src="`${siteUrl.site_url}/media/content/images/${hmsport?.img_bg_path}`"
                                     class="mx-auto w-full group-hover:scale-110 duration-300"
                                     :placeholder="img(`${siteUrl?.site_url}/logo/placeholder.jpg`)" />
                             </NuxtLink>
                         </div>
                         <div class=" col-span-7">
-                            <NuxtLink :to="`/category/${hmsport?.category?.cat_slug}/${hmsport?.content_id}`">
+                            <NuxtLink :to="`/category/${hmsport?.cat_slug}/${hmsport?.content_id}`">
                                 <h4 class="text-[18px] leading-tight group-hover:text-[#ff0000]">{{
                                     hmsport?.content_heading }}</h4>
                             </NuxtLink>
@@ -57,8 +57,12 @@
 const img = useImage()
 const siteUrl = siteUrlState()
 const sportscontent = useState(() => [])
-const { data: hsport } = await useFetch('/api/home/sportscontent', {
-    method: 'GET'
+const { data: hsport } = await useFetch('/api/prismaapi/home/positioncontent', {
+    method: 'POST',
+    body: {
+        cat_id: 5, // assign Category ID
+        take: 5
+    }
 })
 sportscontent.value = hsport
 </script>
