3fb9a4e84c0adc7e96c629715f886d277ed78cc4
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-06 15:10:55 +0600

    education  moment js bn updated

--- server/api/prismaapi/home/education.get.ts
+++ server/api/prismaapi/home/education.get.ts
@@ -1,5 +1,7 @@
+import moment from "moment"
 import { Prisma, PrismaClient } from '@prisma/client'
 export default defineEventHandler(async (event) => {
+    moment.locale('bn-bd')
 
     const prisma = new PrismaClient()
     // const getBody = await readBody(event)
@@ -39,7 +41,8 @@ export default defineEventHandler(async (event) => {
                 content_details: categoryContent[i]?.content_details,
                 bn_cat_name: category?.cat_name_bn,
                 cat_slug: category?.cat_slug,
-                subcat_slug: subcategory?.subcat_slug
+                subcat_slug: subcategory?.subcat_slug,
+                created_at: moment(categoryContent[i]?.created_at).startOf('hour').fromNow()
             })
         }
 
