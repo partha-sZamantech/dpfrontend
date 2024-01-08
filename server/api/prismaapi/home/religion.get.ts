0106183f4e7737b4e8c18a4701e325f920291b89
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-08 12:46:23 +0600

    Religion  home content

--- server/api/prismaapi/home/religion.get.ts
+++ server/api/prismaapi/home/religion.get.ts
@@ -1,10 +1,11 @@
 import moment from "moment"
-import { Prisma, PrismaClient } from '@prisma/client'
+// import { Prisma, PrismaClient } from '@prisma/client'
+import { prisma } from "~/lib/prisma"
 export default defineEventHandler(async (event) => {
     // Localize
     moment.locale('bn-bd')
 
-    const prisma = new PrismaClient()
+    // const prisma = new PrismaClient()
     // const getBody = await readBody(event)
     const categoryContent = await prisma.bn_contents.findMany({
         where: {
