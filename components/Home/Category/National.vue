952e1c857a0ce12c005463accc28b132c36ac953
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-09 15:52:17 +0600

    Home naational category content linked updated

--- components/Home/Category/National.vue
+++ components/Home/Category/National.vue
@@ -9,7 +9,7 @@
         </div>
         <div class="grid grid-cols-12 gap-4">
             <div class="col-span-12 md:col-span-7">
-                <NuxtLink :to="`/category/${nationalHCon[0]?.cat_slug}/${nationalHCon[0]?.content_id}`"
+                <NuxtLink :to="getPostUrl(nationalHCon[0]?.cat_slug, nationalHCon[0]?.subcat_slug, nationalHCon[0]?.content_type, nationalHCon[0]?.content_id)"
                     class="flex flex-col group gap-2">
                     <div class="national-feature-image overflow-hidden">
                         <nuxt-img :src="`${siteurl.site_url}/media/content/images/${nationalHCon[0]?.img_bg_path}`"
@@ -37,14 +37,14 @@
                     <div v-for="nationalcntent in nationalHCon.slice(1, 5)" :key="nationalcntent.content_id"
                         class="grid grid-cols-12 gap-4 group h-national-excpt border-b py-4">
                         <div class=" col-span-5 overflow-hidden">
-                            <NuxtLink :to="`/category/${nationalcntent?.cat_slug}/${nationalcntent?.content_id}`">
+                            <NuxtLink :to="getPostUrl(nationalcntent?.cat_slug, nationalcntent?.subcat_slug, nationalcntent?.content_type, nationalcntent?.content_id)">
                                 <nuxt-img :src="`${siteurl.site_url}/media/content/images/${nationalcntent?.img_bg_path}`"
                                     class="mx-auto w-full group-hover:scale-110 duration-300"
                                     :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)" />
                             </NuxtLink>
                         </div>
                         <div class=" col-span-7">
-                            <NuxtLink class="flex flex-col gap-2" :to="`/category/${nationalcntent?.cat_slug}/${nationalcntent?.content_id}`">
+                            <NuxtLink class="flex flex-col gap-2" :to="getPostUrl(nationalcntent?.cat_slug, nationalcntent?.subcat_slug, nationalcntent?.content_type, nationalcntent?.content_id)">
                                 <h4 class="text-[18px] leading-tight group-hover:text-[#ff0000]">{{
                                     nationalcntent?.content_heading }}</h4>
                                     <span class="text-sm">{{ nationalcntent?.bn_cat_name }} | {{ nationalcntent?.created_at }}</span>
@@ -69,6 +69,13 @@ const { data: nationalhc } = await useFetch('/api/prismaapi/home/national', {
     // cache: 'force-cache',
 })
 nationalHCon.value = nationalhc
+
+// ======== Post Url Generate ============ //
+const getPostUrl = (category_slug, subcategory_slug, content_type, content_id) => {
+    return `/${category_slug}/${subcategory_slug ? subcategory_slug : (content_type === 1 ? 'news' : 'article')}/${content_id}`
+}
+// ======== Post Url Generate ============ //
+
 </script>
 
 <style scoped>.h-national-excpt:first-child {
