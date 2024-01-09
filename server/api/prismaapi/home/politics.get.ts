28112a4dd1c87079567c1c71155332a2495ad403
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-09 15:54:10 +0600

    Home politics api content type added

--- server/api/prismaapi/home/politics.get.ts
+++ server/api/prismaapi/home/politics.get.ts
@@ -50,6 +50,7 @@ export default defineEventHandler(async (event) => {
             })
             data.push({
                 content_id: content?.content_id,
+                content_type: content?.content_type,
                 img_bg_path: content?.img_bg_path,
                 content_heading: content?.content_heading,
                 content_details: content?.content_details,
