7ada0a4a75102e66071eb46d1a2400e2a6aad4c8
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-10 16:09:27 +0600

    Design fixed

--- pages/[category]/[subcategory]/index.vue
+++ pages/[category]/[subcategory]/index.vue
@@ -15,7 +15,7 @@
         <!-- Space For Ads -->
         <div class=" max-w-[1280px] mx-auto category-content px-4 md:px-2 py-4 relative">
             <!-- Breadcrump Section -->
-            <div class="breadcrump border-b border-b-[#dee2e6] pb-2 mb-5 flex flex-col gap-2 md:gap-2">
+            <div class="breadcrump border-b border-b-[#dee2e6] pb-2 py-2 mb-5 flex flex-col gap-2 md:gap-2">
                 <div class="flex gap-1 justify-start items-center">
                     <NuxtLink :to="`/${category?.cat_slug}`" class=" font-semibold">
                         <!-- {{ detailsContent?.category?.cat_name_bn }} -->
@@ -25,7 +25,7 @@
                     </NuxtLink>
                 </div>
 
-                <div class="flex flex-col gap-4">
+                <div class="flex flex-col gap-1">
                     <NuxtLink :to="`/${category?.cat_slug}/${subcat_slug}`" class="text-[#3375af] font-semibold">
                         <!-- {{ detailsContent?.category?.cat_name_bn }} -->
                         <h1 class="text-xl md:text-3xl">{{ category.subcat_name_bn }}</h1>
--- pages/[category]/index.vue
+++ pages/[category]/index.vue
@@ -13,7 +13,7 @@
         <div class=" max-w-[1280px] mx-auto category-content px-4 md:px-2 py-4 relative">
 
             <!-- Breadcrump Section -->
-            <div class="breadcrump border-b border-b-[#dee2e6] pb-2 mb-5 flex flex-col gap-2 md:gap-4">
+            <div class="breadcrump border-b border-b-[#dee2e6] pb-2 mb-5 py-2 flex flex-col gap-0 md:gap-2">
                 <div class="flex gap-1 justify-start items-center">
                     <NuxtLink :to="`/${category?.cat_slug}`" class="text-[#3375af] font-semibold">
                         <!-- {{ detailsContent?.category?.cat_name_bn }} -->
