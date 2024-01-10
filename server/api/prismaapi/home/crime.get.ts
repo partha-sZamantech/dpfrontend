479ec2aa26ae3f7c41c0fbc6a9fe5c667e26498d
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-10 11:56:09 +0600

    Home Crime Category contents

--- server/api/prismaapi/home/crime.get.ts
+++ server/api/prismaapi/home/crime.get.ts
@@ -8,6 +8,8 @@ export default defineEventHandler(async (event) => {
     const categoryContent = await prisma.bn_contents.findMany({
         where: {
             cat_id: 33, // Assign Category ID
+            status: 1,
+            deletable: 1
         },
         take: 4,
         orderBy: {
