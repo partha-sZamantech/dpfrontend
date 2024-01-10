418fbadb3bbc79c8b696b3a73e427efae7d7e03f
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-10 12:03:38 +0600

    Home Religion Category contents

--- server/api/prismaapi/home/religion.get.ts
+++ server/api/prismaapi/home/religion.get.ts
@@ -10,6 +10,8 @@ export default defineEventHandler(async (event) => {
     const categoryContent = await prisma.bn_contents.findMany({
         where: {
             cat_id: 17, // Assign Category ID
+            status: 1,
+            deletable: 1
         },
         take: 5,
         orderBy: {
