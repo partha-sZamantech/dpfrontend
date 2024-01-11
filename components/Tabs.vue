0db8d97a9f7c7d3ae9705b58341a0a84a0920466
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-11 15:05:03 +0600

    Lates

--- components/Tabs.vue
+++ components/Tabs.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="home-post-tabs">
-        <div class="grid grid-cols-2 text-[17px] text-center border-b border-b-[#0000006b]">
-            <div v-if="isActiveStatus === false" :class="` border-b-2 border-b-[#3375af]`"><b>সর্বশেষ</b></div>
-            <div v-else @click="tabToggleHandler" :class="`hover:border-b-[#3375af]  cursor-pointer`"><b>সর্বশেষ</b>
+        <div class="grid grid-cols-2 text-[20px] text-center border-b border-b-[#0000006b]">
+            <div v-if="isActiveStatus === false" :class="` border-b-2 border-b-[#3375af] font-semibold`"><b>সর্বশেষ</b></div>
+            <div v-else @click="tabToggleHandler" :class="`hover:border-b-[#3375af]  cursor-pointer font-semibold`"><b>সর্বশেষ</b>
             </div>
 
-            <div v-if="isActiveStatus === true" :class="`border-b-2 border-b-[#3375af]  py-1`">
+            <div v-if="isActiveStatus === true" :class="`border-b-2 border-b-[#3375af]  py-1 font-semibold`">
                 <b>সর্বাধিক পঠিত</b>
             </div>
-            <div v-else @click="tabToggleHandler" :class="`hover:border-b-[#3375af] cursor-pointer py-1`">
+            <div v-else @click="tabToggleHandler" :class="`hover:border-b-[#3375af] cursor-pointer py-1 font-semibold`">
                 <b>সর্বাধিক পঠিত</b>
             </div>
         </div>
@@ -20,7 +20,7 @@
                 class="latest-post-loop py-3 flex gap-2 border-b border-b-[#e2e2e2] group"
                 v-for="(latstpost, Linx) in latestposts" :key="latstpost?.content_id">
                 <span class="number-count text-[26px] text-[#949494]"><b>{{ toBn(`${Linx + 1}`) }}.</b> </span>
-                <h4 class="text-[20px] group-hover:text-[#ff0000] text-[#121212] leading-6"> {{ latstpost?.content_heading
+                <h4 class="text-base group-hover:text-[#ff0000] text-[#333] font-semibold"> {{ latstpost?.content_heading
                 }}</h4>
 
             </NuxtLink>
@@ -34,7 +34,7 @@
                 class="latest-post-loop py-3 flex gap-2 border-b border-b-[#e2e2e2] group"
                 v-for="(poplarpost, Pinx) in popularposts" :key="poplarpost?.content_id">
                 <span class="number-count text-[26px] text-[#949494]"><b>{{ toBn(`${Pinx + 1}`) }}.</b> </span>
-                <h4 class="text-[20px] group-hover:text-[#ff0000] text-[#121212] leading-6"> {{ poplarpost?.content_heading
+                <h4 class="text-base group-hover:text-[#ff0000] text-[#333] font-semibold"> {{ poplarpost?.content_heading
                 }}</h4>
             </NuxtLink>
 
