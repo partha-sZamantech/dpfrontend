dbeacf6146753742589d7388ea25a600b8656926
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-13 11:34:29 +0600

    lazy loading added

--- components/Home/Category/Campus.vue
+++ components/Home/Category/Campus.vue
@@ -8,25 +8,28 @@
         </div>
         <div class="home-campus-c-content flex flex-col gap-3">
             <!-- Campus Feature Content -->
-            <NuxtLink :to="getPostUrl(campuses[0]?.cat_slug, campuses[0]?.subcat_slug, campuses[0]?.content_type, campuses[0]?.content_id)" class="flex flex-col gap-2 group border-b pb-1">
+            <NuxtLink
+                :to="getPostUrl(campuses[0]?.cat_slug, campuses[0]?.subcat_slug, campuses[0]?.content_type, campuses[0]?.content_id)"
+                class="flex flex-col gap-2 group border-b pb-1">
                 <div class=" overflow-hidden">
-                    <nuxt-img :src="`${siteurl.site_url}/media/content/images/${campuses[0]?.img_bg_path}`"
-                            class="mx-auto w-full group-hover:scale-110 duration-300"
-                            :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)"  />
+                    <nuxt-img loading="lazy" :src="`${siteurl.site_url}/media/content/images/${campuses[0]?.img_bg_path}`"
+                        class="mx-auto w-full group-hover:scale-110 duration-300"
+                        :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)" />
                 </div>
-                <h3 class="text-[19px] text-black font-semibold group-hover:text-[#ff0000]">{{ campuses[0]?.content_heading }}</h3>
+                <h3 class="text-[19px] text-black font-semibold group-hover:text-[#ff0000]">{{ campuses[0]?.content_heading
+                }}</h3>
                 <span class="text-sm">{{ campuses[0]?.created_at }}</span>
             </NuxtLink>
             <!--/ Campus Feature Content -->
 
             <div class="h-p-c-excpt flex flex-col">
                 <!-- Loop Item -->
-                <NuxtLink :to="getPostUrl(campus?.cat_slug, campus?.subcat_slug, campus?.content_type, campus?.content_id)" class=" border-b py-3" v-for="campus in campuses.slice(1,8)"
-                :key="campus.content_id">
+                <NuxtLink :to="getPostUrl(campus?.cat_slug, campus?.subcat_slug, campus?.content_type, campus?.content_id)"
+                    class=" border-b py-3" v-for="campus in campuses.slice(1, 8)" :key="campus.content_id">
                     <h4 class="text-base text-black font-semibold hover:text-[#ff0000]">{{ campus?.content_heading }}</h4>
                 </NuxtLink>
                 <!--/ Loop Item -->
-        
+
 
             </div>
 
