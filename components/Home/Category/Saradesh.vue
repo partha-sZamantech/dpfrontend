cc0c421245bce12e1784b225cd8bc0a5c5a533e3
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-13 11:27:24 +0600

    lazy loading added

--- components/Home/Category/Saradesh.vue
+++ components/Home/Category/Saradesh.vue
@@ -11,15 +11,20 @@
             <div class="grid grid-cols-12 gap-4 group h-sports-excpt border-b py-4" v-for="saradeshcon in saradeshcontents"
                 :key="saradeshcon.content_id">
                 <div class=" col-span-5 overflow-hidden">
-                    <NuxtLink :to="getPostUrl(saradeshcon?.cat_slug, saradeshcon?.subcat_slug, saradeshcon?.content_type, saradeshcon?.content_id)">
-                        <nuxt-img :src="`${siteUrl.site_url}/media/content/images/${saradeshcon?.img_bg_path}`"
+                    <NuxtLink
+                        :to="getPostUrl(saradeshcon?.cat_slug, saradeshcon?.subcat_slug, saradeshcon?.content_type, saradeshcon?.content_id)">
+                        <nuxt-img loading="lazy"
+                            :src="`${siteUrl.site_url}/media/content/images/${saradeshcon?.img_bg_path}`"
                             class="mx-auto w-full group-hover:scale-110 duration-300"
                             :placeholder="img(`${siteUrl?.site_url}/logo/placeholder.jpg`)" />
                     </NuxtLink>
                 </div>
                 <div class=" col-span-7">
-                    <NuxtLink :to="getPostUrl(saradeshcon?.cat_slug, saradeshcon?.subcat_slug, saradeshcon?.content_type, saradeshcon?.content_id)" class="flex flex-col gap-2">
-                        <h4 class="text-base text-black font-semibold group-hover:text-[#ff0000]">{{ saradeshcon?.content_heading
+                    <NuxtLink
+                        :to="getPostUrl(saradeshcon?.cat_slug, saradeshcon?.subcat_slug, saradeshcon?.content_type, saradeshcon?.content_id)"
+                        class="flex flex-col gap-2">
+                        <h4 class="text-base text-black font-semibold group-hover:text-[#ff0000]">{{
+                            saradeshcon?.content_heading
                         }}</h4>
                         <span class="text-sm text-black">{{ saradeshcon?.created_at }}</span>
                     </NuxtLink>
@@ -49,8 +54,6 @@ const getPostUrl = (category_slug, subcategory_slug, content_type, content_id) =
 // ======== Post Url Generate ============ //
 </script>
 
-<style scoped>
-.h-sports-excpt:first-child {
+<style scoped>.h-sports-excpt:first-child {
     padding-top: 0px
-}
-</style>
+}</style>
