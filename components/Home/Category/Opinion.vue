7773c93f375df5a654babca0839e1d9d66d185fd
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-13 11:38:22 +0600

    lazy loading added

--- components/Home/Category/Opinion.vue
+++ components/Home/Category/Opinion.vue
@@ -8,20 +8,28 @@
         </div>
         <div class="home-int-c-content flex flex-col gap-3">
             <!-- Opinion Feature Content -->
-            <NuxtLink :to="getPostUrl(opinionContents[0]?.cat_slug, opinionContents[0]?.subcat_slug, opinionContents[0]?.content_type, opinionContents[0]?.content_id)" class="flex flex-col gap-2 group">
+            <NuxtLink
+                :to="getPostUrl(opinionContents[0]?.cat_slug, opinionContents[0]?.subcat_slug, opinionContents[0]?.content_type, opinionContents[0]?.content_id)"
+                class="flex flex-col gap-2 group">
                 <div class=" overflow-hidden">
-                    <nuxt-img :src="`${siteurl.site_url}/media/content/images/${opinionContents[0]?.img_bg_path}`"
-                            class="mx-auto w-full group-hover:scale-110 duration-300"
-                            :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)"  />
+                    <nuxt-img loading="lazy"
+                        :src="`${siteurl.site_url}/media/content/images/${opinionContents[0]?.img_bg_path}`"
+                        class="mx-auto w-full group-hover:scale-110 duration-300"
+                        :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)" />
                 </div>
-                <h3 class="text-[19px] text-black font-semibold group-hover:text-[#ff0000]">{{ opinionContents[0]?.content_heading }}</h3>
+                <h3 class="text-[19px] text-black font-semibold group-hover:text-[#ff0000]">{{
+                    opinionContents[0]?.content_heading }}</h3>
             </NuxtLink>
             <!--/ Opinion Feature Content -->
 
             <div class="h-p-c-excpt flex flex-col">
                 <!-- Loop Item -->
-                <NuxtLink :to="getPostUrl(opinionContent?.cat_slug, opinionContent?.subcat_slug, opinionContent?.content_type, opinionContent?.content_id)" class=" border-b py-3" v-for="opinionContent in opinionContents.slice(1, 7)" :key="opinionContent.content_id">
-                    <h4 class="text-base hover:text-[#ff0000] text-black font-semibold">{{ opinionContent?.content_heading }}</h4>
+                <NuxtLink
+                    :to="getPostUrl(opinionContent?.cat_slug, opinionContent?.subcat_slug, opinionContent?.content_type, opinionContent?.content_id)"
+                    class=" border-b py-3" v-for="opinionContent in opinionContents.slice(1, 7)"
+                    :key="opinionContent.content_id">
+                    <h4 class="text-base hover:text-[#ff0000] text-black font-semibold">{{ opinionContent?.content_heading
+                    }}</h4>
                 </NuxtLink>
                 <!--/ Loop Item -->
 
@@ -40,7 +48,7 @@ const opinionContents = useState(() => [])
 const { data: hmOpinion } = await useFetch("/api/prismaapi/home/opinion", {
     method: 'GET',
     // cache: 'force-cache',
- 
+
 })
 opinionContents.value = hmOpinion
 // ======== Opinion Content =============== //
