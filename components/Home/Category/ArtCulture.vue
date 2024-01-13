4df683d1b8bc42afe2ddb932b855148d3898fd30
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-13 11:35:01 +0600

    lazy loading added

--- components/Home/Category/ArtCulture.vue
+++ components/Home/Category/ArtCulture.vue
@@ -8,10 +8,12 @@
         </div>
         <div class="home-int-c-content flex flex-col gap-3">
             <!-- Art Culture Feature Content -->
-            <NuxtLink :to="getPostUrl(artscontents[0]?.cat_slug, artscontents[0]?.subcat_slug, artscontents[0]?.content_type, artscontents[0]?.content_id)"
+            <NuxtLink
+                :to="getPostUrl(artscontents[0]?.cat_slug, artscontents[0]?.subcat_slug, artscontents[0]?.content_type, artscontents[0]?.content_id)"
                 class="flex flex-col gap-2 group border-b pb-1">
                 <div class=" overflow-hidden">
-                    <nuxt-img :src="`${siteurl.site_url}/media/content/images/${artscontents[0]?.img_bg_path}`"
+                    <nuxt-img loading="lazy"
+                        :src="`${siteurl.site_url}/media/content/images/${artscontents[0]?.img_bg_path}`"
                         class="mx-auto w-full group-hover:scale-110 duration-300"
                         :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)" />
                 </div>
@@ -24,9 +26,11 @@
 
             <div class="h-p-c-excpt flex flex-col">
                 <!-- Loop Item -->
-                <NuxtLink :to="getPostUrl(artscontent?.cat_slug, artscontent?.subcat_slug, artscontent?.content_type, artscontent?.content_id)" class=" border-b py-3"
-                    v-for="artscontent in artscontents.slice(1, 5)" :key="artscontent.content_id">
-                    <h4 class="text-base text-black font-semibold hover:text-[#ff0000]">{{ artscontent?.content_heading }}</h4>
+                <NuxtLink
+                    :to="getPostUrl(artscontent?.cat_slug, artscontent?.subcat_slug, artscontent?.content_type, artscontent?.content_id)"
+                    class=" border-b py-3" v-for="artscontent in artscontents.slice(1, 5)" :key="artscontent.content_id">
+                    <h4 class="text-base text-black font-semibold hover:text-[#ff0000]">{{ artscontent?.content_heading }}
+                    </h4>
                 </NuxtLink>
                 <!--/ Loop Item -->
 
