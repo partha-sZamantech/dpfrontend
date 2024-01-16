3c89140f649f72ef99014d4d8b6c40a66f24eb02
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-16 18:44:43 +0600

    sdfsdf

--- components/Home/Category/National.vue
+++ components/Home/Category/National.vue
@@ -9,24 +9,27 @@
         </div>
         <div class="grid grid-cols-12 gap-4">
             <div class="col-span-12 md:col-span-7">
-                <NuxtLink :to="getPostUrl(nationalHCon[0]?.cat_slug, nationalHCon[0]?.subcat_slug, nationalHCon[0]?.content_type, nationalHCon[0]?.content_id)"
+                <NuxtLink
+                    :to="getPostUrl(nationalHCon[0]?.cat_slug, nationalHCon[0]?.subcat_slug, nationalHCon[0]?.content_type, nationalHCon[0]?.content_id)"
                     class="flex flex-col group gap-2">
                     <div class="national-feature-image overflow-hidden">
-                        <nuxt-img loading="lazy" :src="`${siteurl.site_url}/media/content/images/${nationalHCon[0]?.img_bg_path}`"
+                        <nuxt-img loading="lazy"
+                            :src="`${siteurl.site_url}/media/content/images/${nationalHCon[0]?.img_bg_path}`"
                             class="mx-auto w-full group-hover:scale-110 duration-300"
-                            :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)"  />
+                            :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)" />
                     </div>
                     <div class="national-feature-description flex flex-col gap-2">
-                        <h3 class="text-[25px] leading-tight font-semibold group-hover:text-[#ff0000]">{{ nationalHCon[0]?.content_heading
+                        <h3 class="text-[25px] leading-tight font-semibold group-hover:text-[#ff0000]">{{
+                            nationalHCon[0]?.content_heading
                         }}</h3>
                         <ClientOnly>
                             <!-- <p class="text-md">{{ nationalHCon[0]?.content_details?.substring(0,
                                 200)?.toString().replace(/(<([^>]+)>)/ig, '') }}...</p> -->
-                                <div class="text-[16px] font-[300] text-black" v-html="`${nationalHCon[0]?.content_details?.substring(0,
-                                220)} ...`"></div>
+                            <div class="text-[16px] font-[300] text-black" v-html="`${nationalHCon[0]?.content_details?.substring(0,
+                                        220)} ...`"></div>
                         </ClientOnly>
                         <span class="text-sm text-black">
-                            {{ nationalHCon[0]?.bn_cat_name }} | {{ postCreatedDate(nationalHCon[0]?.created_at) }}
+                            {{ postCreatedDate(nationalHCon[0]?.created_at) }}
                         </span>
                     </div>
                 </NuxtLink>
@@ -37,17 +40,20 @@
                     <div v-for="nationalcntent in nationalHCon.slice(1, 5)" :key="nationalcntent.content_id"
                         class="grid grid-cols-12 gap-4 group h-national-excpt border-b py-4">
                         <div class=" col-span-5 overflow-hidden">
-                            <NuxtLink :to="getPostUrl(nationalcntent?.cat_slug, nationalcntent?.subcat_slug, nationalcntent?.content_type, nationalcntent?.content_id)">
-                                <nuxt-img loading="lazy" :src="`${siteurl.site_url}/media/content/images/${nationalcntent?.img_bg_path}`"
+                            <NuxtLink
+                                :to="getPostUrl(nationalcntent?.cat_slug, nationalcntent?.subcat_slug, nationalcntent?.content_type, nationalcntent?.content_id)">
+                                <nuxt-img loading="lazy"
+                                    :src="`${siteurl.site_url}/media/content/images/${nationalcntent?.img_bg_path}`"
                                     class="mx-auto w-full group-hover:scale-110 duration-300"
                                     :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)" />
                             </NuxtLink>
                         </div>
                         <div class=" col-span-7">
-                            <NuxtLink class="flex flex-col gap-2" :to="getPostUrl(nationalcntent?.cat_slug, nationalcntent?.subcat_slug, nationalcntent?.content_type, nationalcntent?.content_id)">
+                            <NuxtLink class="flex flex-col gap-2"
+                                :to="getPostUrl(nationalcntent?.cat_slug, nationalcntent?.subcat_slug, nationalcntent?.content_type, nationalcntent?.content_id)">
                                 <h4 class="text-base font-semibold group-hover:text-[#ff0000]">{{
                                     nationalcntent?.content_heading }}</h4>
-                                    <span class="text-sm text-black">{{ nationalcntent?.bn_cat_name }} | {{ postCreatedDate(nationalcntent?.created_at) }}</span>
+                                <span class="text-sm text-black">{{ postCreatedDate(nationalcntent?.created_at) }}</span>
                             </NuxtLink>
                         </div>
                     </div>
