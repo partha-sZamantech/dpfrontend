9171c607b011c68f86e3dbf85b91a63f81f5acde
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-20 15:16:56 +0600

    Law court

--- components/Home/Category/ChildAdolescent.vue
+++ components/Home/Category/ChildAdolescent.vue
@@ -8,26 +8,35 @@
         </div>
         <div class="home-int-c-content flex flex-col gap-3">
             <!-- Children Feature Content -->
-            <NuxtLink :to="getPostUrl(childrenContents[0]?.cat_slug, childrenContents[0]?.subcat_slug, childrenContents[0]?.content_type, childrenContents[0]?.content_id)"
+            <NuxtLink
+                :to="getPostUrl(childrenContents[0]?.cat_slug, childrenContents[0]?.subcat_slug, childrenContents[0]?.content_type, childrenContents[0]?.content_id)"
                 class="flex flex-col gap-2 group border-b pb-1">
                 <div class=" overflow-hidden">
-                    <nuxt-img loading="lazy" :src="`${siteurl.site_url}/media/content/images/${childrenContents[0]?.img_bg_path}`"
+                    <nuxt-img loading="lazy"
+                        :src="`${siteurl.site_url}/media/content/images/${childrenContents[0]?.img_bg_path}`"
                         class="mx-auto w-full group-hover:scale-110 duration-300"
                         :placeholder="img(`${siteurl?.site_url}/logo/placeholder.jpg`)" />
                 </div>
                 <h3 class="text-[19px] text-black font-semibold group-hover:text-[#ff0000]">
                     {{ childrenContents[0]?.content_heading }}
                 </h3>
-                <span class="text-sm text-black">{{ postCreatedDate(childrenContents[0]?.created_at) }}</span>
+                <p class="text-sm text-black flex gap-1 items-center">
+                    <Icon name="ph:alarm-bold" />
+                    <span>
+                        {{ postCreatedDate(childrenContents[0]?.created_at) }}
+                    </span>
+                </p>
             </NuxtLink>
             <!--/ Children Feature Content -->
 
             <div class="h-p-c-excpt flex flex-col">
                 <!-- Loop Item -->
-                <NuxtLink :to="getPostUrl(childrenContent?.cat_slug, childrenContent?.subcat_slug, childrenContent?.content_type, childrenContent?.content_id)"
+                <NuxtLink
+                    :to="getPostUrl(childrenContent?.cat_slug, childrenContent?.subcat_slug, childrenContent?.content_type, childrenContent?.content_id)"
                     class=" border-b py-3" v-for="childrenContent in childrenContents.slice(1, 5)"
                     :key="childrenContent.content_id">
-                    <h4 class="text-base text-black font-semibold hover:text-[#ff0000]">{{ childrenContent?.content_heading }}</h4>
+                    <h4 class="text-base text-black font-semibold hover:text-[#ff0000]">{{ childrenContent?.content_heading
+                    }}</h4>
                 </NuxtLink>
                 <!--/ Loop Item -->
             </div>
