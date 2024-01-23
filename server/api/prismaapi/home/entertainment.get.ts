860a649ea64330112407fcb594a33aadeb0c631a
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-23 14:07:43 +0600

    Special top content fixed

--- server/api/prismaapi/home/entertainment.get.ts
+++ server/api/prismaapi/home/entertainment.get.ts
@@ -28,6 +28,7 @@ export default defineEventHandler(async (event) => {
         const positionArray = position?.content_ids?.split(',')
         const getContents = positionArray?.splice(0, 5)
         for (let i = 0; i < getContents?.length; i++) {
+            
             const content = await prisma.bn_contents.findFirst({
                 where: {
                     content_id: parseInt(getContents[i]),
@@ -36,32 +37,37 @@ export default defineEventHandler(async (event) => {
                 }
             })
 
-            // Category GET
-            const category = await prisma.bn_categories.findFirst({
-                where: {
-                    cat_id: content?.cat_id,
-                    cat_type: 1
-                }
-            })
+            // If Content Has
+            if (content) {
+                // Category GET
+                const category = await prisma.bn_categories.findFirst({
+                    where: {
+                        cat_id: content?.cat_id,
+                        cat_type: 1
+                    }
+                })
 
-            // Subcategory get
-            const subcategory = await prisma.bn_subcategories.findFirst({
-                where: {
-                    subcat_id: content?.subcat_id
-                }
-            })
-            data.push({
-                content_id: content?.content_id,
-                content_type: content?.content_type,
-                img_bg_path: content?.img_bg_path,
-                content_heading: content?.content_heading,
-                content_details: content?.content_details,
-                bn_cat_name: category?.cat_name_bn,
-                cat_slug: category?.cat_slug,
-                subcat_slug: subcategory?.subcat_slug,
-                created_at: content?.created_at
-                // created_at: moment(content?.created_at).startOf('hour').fromNow()
-            })
+                // Subcategory get
+                const subcategory = await prisma.bn_subcategories.findFirst({
+                    where: {
+                        subcat_id: content?.subcat_id
+                    }
+                })
+                data.push({
+                    content_id: content?.content_id,
+                    content_type: content?.content_type,
+                    img_bg_path: content?.img_bg_path,
+                    content_heading: content?.content_heading,
+                    content_details: content?.content_details,
+                    bn_cat_name: category?.cat_name_bn,
+                    cat_slug: category?.cat_slug,
+                    subcat_slug: subcategory?.subcat_slug,
+                    created_at: content?.created_at
+                    // created_at: moment(content?.created_at).startOf('hour').fromNow()
+                })
+
+            }
+
         }
 
         return data
