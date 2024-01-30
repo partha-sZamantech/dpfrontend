18518d86c2e74e5fec08a6ce47db6d0af229d99f
Parthadeb Mondal <Parthadev76@gmail.com>
2024-01-30 12:32:20 +0600

    Dhaka prokash mobile

--- components/Home/Category/Sports.vue
+++ components/Home/Category/Sports.vue
@@ -36,7 +36,7 @@
             </div>
             <div class="col-span-12 md:col-span-6">
                 <!-- Desktop Version -->
-                <div class=" md:grid grid-cols-2 gap-3 hidden">
+                <!-- <div class=" md:grid grid-cols-2 gap-3 hidden">
                     <NuxtLink v-for="(leftsport, lidx) in sportscontent?.slice(1, 5)" :key="lidx"
                         :to="getPostUrl(leftsport?.cat_slug, leftsport?.subcat_slug, leftsport?.content_type, leftsport?.content_id)"
                         class=" flex flex-col gap-2 group">
@@ -57,10 +57,123 @@
                             </p>
                         </div>
                     </NuxtLink>
-                </div>
+                </div> -->
                 <!-- Desktop Version -->
+
                 <!-- Mobile Version -->
-                <!-- Mobile Version -->'
+                <div class="grid grid-cols-2 gap-4 mb-2 mt-4">
+                    <NuxtLink
+                        :to="getPostUrl(sportscontent[1]?.cat_slug, sportscontent[1]?.subcat_slug, sportscontent[1]?.content_type, sportscontent[1]?.content_id)"
+                        class="group">
+                        <div class=" overflow-hidden">
+                            <nuxt-img loading="lazy"
+                                :src="`${siteUrl?.site_url}/media/content/images/${sportscontent[1]?.img_bg_path}`"
+                                class="mx-auto w-full group-hover:scale-110 duration-300"
+                                :placeholder="img(`${siteUrl?.site_url}/logo/placeholder.jpg`)" />
+                        </div>
+                    </NuxtLink>
+                    <NuxtLink class="group"
+                        :to="getPostUrl(sportscontent[2]?.cat_slug, sportscontent[2]?.subcat_slug, sportscontent[2]?.content_type, sportscontent[2]?.content_id)">
+                        <div class=" overflow-hidden">
+                            <nuxt-img loading="lazy"
+                                :src="`${siteUrl?.site_url}/media/content/images/${sportscontent[2]?.img_bg_path}`"
+                                class="mx-auto w-full group-hover:scale-110 duration-300"
+                                :placeholder="img(`${siteUrl?.site_url}/logo/placeholder.jpg`)" />
+                        </div>
+                    </NuxtLink>
+                </div>
+                <div class="grid grid-cols-2 gap-4">
+                    <NuxtLink class="group"
+                        :to="getPostUrl(sportscontent[1]?.cat_slug, sportscontent[1]?.subcat_slug, sportscontent[1]?.content_type, sportscontent[1]?.content_id)">
+                        <h4 class=" text-base text-black group-hover:text-[#ff0000] font-semibold">{{
+                            sportscontent[1]?.content_heading
+                        }}</h4>
+                    </NuxtLink>
+                    <NuxtLink class="group"
+                        :to="getPostUrl(sportscontent[2]?.cat_slug, sportscontent[2]?.subcat_slug, sportscontent[2]?.content_type, sportscontent[2]?.content_id)">
+                        <h4 class=" text-base text-black group-hover:text-[#ff0000] font-semibold ">{{
+                            sportscontent[2]?.content_heading
+                        }}</h4>
+                    </NuxtLink>
+                </div>
+
+                <div class="grid grid-cols-2 gap-4">
+                    <NuxtLink
+                        :to="getPostUrl(sportscontent[1]?.cat_slug, sportscontent[1]?.subcat_slug, sportscontent[1]?.content_type, sportscontent[1]?.content_id)"
+                        class="text-sm text-black mt-1 flex gap-1 items-center">
+                        <Icon name="ph:alarm-bold" />
+                        <span>
+                            {{ postCreatedDate(sportscontent[1]?.created_at) }}
+                        </span>
+                    </NuxtLink>
+                    <NuxtLink
+                        :to="getPostUrl(sportscontent[2]?.cat_slug, sportscontent[2]?.subcat_slug, sportscontent[2]?.content_type, sportscontent[2]?.content_id)"
+                        class="text-sm text-black mt-1 flex gap-1 items-center">
+                        <Icon name="ph:alarm-bold" />
+                        <span>
+                            {{ postCreatedDate(sportscontent[2]?.created_at) }}
+                        </span>
+                    </NuxtLink>
+
+                </div>
+
+
+                <div class="grid grid-cols-2 gap-4 mb-2 mt-4">
+                    <NuxtLink
+                        :to="getPostUrl(sportscontent[3]?.cat_slug, sportscontent[3]?.subcat_slug, sportscontent[3]?.content_type, sportscontent[3]?.content_id)"
+                        class="group">
+                        <div class=" overflow-hidden">
+                            <nuxt-img loading="lazy"
+                                :src="`${siteUrl?.site_url}/media/content/images/${sportscontent[3]?.img_bg_path}`"
+                                class="mx-auto w-full group-hover:scale-110 duration-300"
+                                :placeholder="img(`${siteUrl?.site_url}/logo/placeholder.jpg`)" />
+                        </div>
+                    </NuxtLink>
+                    <NuxtLink class="group"
+                        :to="getPostUrl(sportscontent[4]?.cat_slug, sportscontent[4]?.subcat_slug, sportscontent[4]?.content_type, sportscontent[4]?.content_id)">
+                        <div class=" overflow-hidden">
+                            <nuxt-img loading="lazy"
+                                :src="`${siteUrl?.site_url}/media/content/images/${sportscontent[4]?.img_bg_path}`"
+                                class="mx-auto w-full group-hover:scale-110 duration-300"
+                                :placeholder="img(`${siteUrl?.site_url}/logo/placeholder.jpg`)" />
+                        </div>
+                    </NuxtLink>
+                </div>
+                <div class="grid grid-cols-2 gap-4">
+                    <NuxtLink class="group"
+                        :to="getPostUrl(sportscontent[3]?.cat_slug, sportscontent[3]?.subcat_slug, sportscontent[3]?.content_type, sportscontent[3]?.content_id)">
+                        <h4 class=" text-base text-black group-hover:text-[#ff0000] font-semibold">{{
+                            sportscontent[3]?.content_heading
+                        }}</h4>
+                    </NuxtLink>
+                    <NuxtLink class="group"
+                        :to="getPostUrl(sportscontent[4]?.cat_slug, sportscontent[4]?.subcat_slug, sportscontent[4]?.content_type, sportscontent[4]?.content_id)">
+                        <h4 class=" text-base text-black group-hover:text-[#ff0000] font-semibold ">{{
+                            sportscontent[4]?.content_heading
+                        }}</h4>
+                    </NuxtLink>
+                </div>
+
+                <div class="grid grid-cols-2 gap-4">
+                    <NuxtLink
+                        :to="getPostUrl(sportscontent[3]?.cat_slug, sportscontent[3]?.subcat_slug, sportscontent[3]?.content_type, sportscontent[3]?.content_id)"
+                        class="text-sm text-black mt-1 flex gap-1 items-center">
+                        <Icon name="ph:alarm-bold" />
+                        <span>
+                            {{ postCreatedDate(sportscontent[3]?.created_at) }}
+                        </span>
+                    </NuxtLink>
+                    <NuxtLink
+                        :to="getPostUrl(sportscontent[4]?.cat_slug, sportscontent[4]?.subcat_slug, sportscontent[4]?.content_type, sportscontent[4]?.content_id)"
+                        class="text-sm text-black mt-1 flex gap-1 items-center">
+                        <Icon name="ph:alarm-bold" />
+                        <span>
+                            {{ postCreatedDate(sportscontent[4]?.created_at) }}
+                        </span>
+                    </NuxtLink>
+
+                </div>
+                <!-- Mobile Version -->
             </div>
 
         </div>
